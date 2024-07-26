1912f7b00da1b5e02ec9c509fe2f5a1d44e6e4db
GossipCoco <gossipcoco@hotmail.fr>
2024-07-26 15:02:57 +0200

    fix bug

--- src/components/Fictions/AllMyFictions/AllFictionsLayout.vue
+++ src/components/Fictions/AllMyFictions/AllFictionsLayout.vue
@@ -1,39 +1,7 @@
 <template>
   <div class="dashboard-max-card-container card fiction-container">
-    <div class="card-header">
-      <h4>Lire mes fictions de {{ usrId }}</h4>
-    </div>
-    <div class="row list-fiction-card-container">
-      <div class="col-4 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4"></div>
-
-      <div class="col-4 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4"></div>
-
-      <div class="col-4 col-sm-12 col-md-12 col-lg-4 col-xl-34 col-xxl-4 mb-4 mb-sm-4 mb-4 mb-sm-4"
-
-        v-for="(game, index) in games" :key="index">
-        <div class="card mb-3" style="max-width: 540px;" v-for="(fiction, index) in game.Fiction" :key="index">
-          <div class="row g-0">
-            <div class="col-md-6">
-              <div class="image-fiction-container"
-                v-bind:style="{ backgroundImage: 'url(/images/Fictions/' + fiction.Image + ')' }">
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="card-body">
-                <h5 class="card-title" style="color: black">{{ fiction.Title }}</h5>
-                <p class="card-text" style="color: black; font-size: 14px" v-html="truncateText(fiction.Summary, 35)">
-                </p>
-                <p class="card-text" style="color: black">
-                  <small class="text-body-secondary">
-                    <router-link class="link-text-a" :to="'/fiction/' + fiction.Title">Lire</router-link>
-                  </small>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <CardHeader v-bind:Title="'Lire toutes mes fictions'" />
+    <AllCardsFictions v-bind:games="games" />
     <div class="row pagination-container">
       <div class="row bottom-top-dashboard">
         <Spinner v-if="showspinner"/>
@@ -43,7 +11,6 @@
     </div>
   </div>
 </template>
-
 <script>
 import GameService from '../../../services/GameService'
 import CardHeader from '../../Components/GenericComponent/CardHeader.vue';
