c425582ea015efc7f992ff1dd16bffc6ec9aa84f
GossipCoco <gossipcoco@hotmail.fr>
2024-08-11 09:54:27 +0200

    update image

--- src/components/Dashboard/Components/TitleHeader.vue
+++ src/components/Dashboard/Components/TitleHeader.vue
@@ -4,8 +4,9 @@
             <h1>Bienvenue sur votre tableau de bord</h1>
              <ImageRondDashboard v-bind:src="'/images/Avatars/' + Avatar" v-bind:alt="Avatar" v-bind:classe="'card-img-top'"/>
              <div class="image-messenger-container">
-                <router-link :to="'/user/messagerie/'+User" >
+                <router-link :to="'/user/messagerie/'+User" class="message-sent-container" >
                     <img src="/images/icons/envelope-solid.svg" />
+                    <div class="mini-rond-messages"><p>{{ NbMessages }}</p></div>
                 </router-link>
             </div>
         </div>
@@ -16,7 +17,7 @@ import ImageRondDashboard from '../../Components/SpecificComponent/ImageRondDash
 export default {
     name: "TitleHeader",
     components: {ImageRondDashboard},
-    props: ['UserName', 'Avatar', 'User']
+    props: ['UserName', 'Avatar', 'User', 'NbMessages']
 
 }
 </script>
