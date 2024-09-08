ee3488d494246f9f70cd560ae7a244e64c924dfc
GossipCoco <gossipcoco@hotmail.fr>
2024-09-08 20:55:17 +0200

    update user-file

--- src/components/Users/Components/UserInformations.vue
+++ src/components/Users/Components/UserInformations.vue
@@ -20,15 +20,22 @@
   >
     <h3>{{ gamer.UserName }}</h3>
     <div class="display-flex-row">
-      <div class="avatar-gamer-container">
-        <img class="avatar-gamer" :src="gamer.Image" />
+      <div class="display-flex-row">
+        <div class="avatar-gamer-container">
+          <p><img class="avatar-gamer" :src="gamer.Image" /><br /></p>
+          <p>Clan de naissance : {{ gamer.ClanId }}</p>
+          <p>Statut actuel : {{ gamer.Status }}</p>
+          <p>Genre du personnage : {{ gamer.Genre }}</p>
+        </div>
+        <div class="informations-gamer-container display-flex">
+          <div class="display-flex-column">
+            <h2>Description</h2>
+            <p v-html="gamer.Description"></p>
+            <h2>Biographie</h2>
+            <p v-html="gamer.Biography"></p>
+          </div>
+        </div>
       </div>
-
-      <p v-html="gamer.ClanId"></p>
-      <p v-html="gamer.Status"></p>
-      <p v-html=" gamer.Genre"></p>
-      <p v-html="gamer.Description"></p>
-      <p v-html="gamer.Biography"></p>
     </div>
   </div>
 </template>
