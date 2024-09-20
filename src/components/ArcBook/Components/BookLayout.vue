49c2a36aec537419cfb5043dbbf8e96d30bb3f12
GossipCoco <gossipcoco@hotmail.fr>
2024-09-20 16:52:32 +0200

    update image + layout book

--- src/components/ArcBook/Components/BookLayout.vue
+++ src/components/ArcBook/Components/BookLayout.vue
@@ -1,71 +1,30 @@
 <template>
-  <div class="row display-flex-row">
-    <div class="col-3 illustration-arc-container">
-      <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              :src="'/images/Books/' + arc.IllustrationOne"
-              :alt="arc.Title"
-              class="d-block w-100"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              :src="'/images/Books/' + arc.IllustrationTwo"
-              :alt="arc.Title"
-              class="d-block w-100"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              :src="'/images/Books/' + arc.IllustrationThree"
-              :alt="arc.Title"
-              class="d-block w-100"
-            />
-          </div>
+  <div class="row">
+    <CarouselArc v-bind:arc="arc" />
+    <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xxl-4">
+      <div
+        class="illustration-arc-container card-image"
+        v-bind:style="{
+          backgroundImage: 'url(/images/Books/' + arc.Image + ')',
+        }"
+      ></div>
+    </div>
+    <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xxl-4">
+      <div class="display-flex-column">
+        <div v-for="(book, index) in arc.Books" :key="index">
+          <p>{{ book.Title }}</p>
         </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-      <!-- <img :src="'/images/Books/' + arc.IllustrationOne" :alt="arc.Title" /> -->
-    </div>
-    <div class="col-3 display-flex-column">
-      <div class="title-arc-Image">
-        <h2>{{ arc.Title }}</h2>
-        <div
-          class="illustration-arc-container card-image"
-          v-bind:style="{
-            backgroundImage: 'url(/images/Books/' + arc.Image + ')',
-          }"
-        ></div>
-      </div>
-      <div v-for="(book, index) in arc.Books" :key="index">
-        <p>{{ book.Title }}</p>
       </div>
     </div>
-    <div class="col-6"></div>
   </div>
 </template>
 <script>
+import CarouselArc from "./CarouselArc.vue";
 export default {
   name: "BookLayout",
   props: ["arc"],
+  components: {
+    CarouselArc,
+  },
 };
 </script>
