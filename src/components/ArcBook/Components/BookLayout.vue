9aa7a472da1a27db67e40d72339913250b214d1c
GossipCoco <gossipcoco@hotmail.fr>
2024-09-21 09:01:19 +0200

    book layout + cover img

--- src/components/ArcBook/Components/BookLayout.vue
+++ src/components/ArcBook/Components/BookLayout.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="row">
     <CarouselArc v-bind:arc="arc" />
-    <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xxl-4">
-      <div
-        class="illustration-arc-container card-image"
-        v-bind:style="{
-          backgroundImage: 'url(/images/Books/' + arc.Image + ')',
-        }"
-      ></div>
-    </div>
-    <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xxl-4">
-      <div class="display-flex-column">
-        <div v-for="(book, index) in arc.Books" :key="index">
-          <p>{{ book.Title }}</p>
-        </div>
-      </div>
-    </div>
+    <div class="col-8 col-sm-12 col-md-8 col-lg-8 col-xxl-8">
+        <h2>{{ arc.Title }}</h2>
+        <div v-html="arc.Summary" class="text-align-justify"></div>        
+    </div>    
   </div>
 </template>
 <script>
