ba8138e8c2f21c4a709344a5b09ed455b39eb583
GossipCoco <gossipcoco@hotmail.fr>
2024-09-21 15:38:56 +0200

    dashboard update

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const Config = {};
-
 // Config.Url = "http://192.168.1.10:4047";
-Config.Url = "http://192.168.1.14:4047"
+// Config.Url = "http://192.168.1.14:4047"
 // Config.Url = "http://192.168.1.23:4047";
 
 
@@ -10,5 +8,25 @@ Config.Url = "http://192.168.1.14:4047"
 // Config.Url = "https://192.168.189.223:4047"
 // Config.Url = "http://192.168.168.153:4047"
 // Config.Url = "http://192.168.1.23:4047"
+
+const os = require('os');
+const Config = {};
+
+function getLocalIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const interfaceName in interfaces) {
+    for (const iface of interfaces[interfaceName]) {
+      const { family, internal, address } = iface;
+      if (family === 'IPv4' && !internal) {
+        return address;
+      }
+    }
+  }
+  return 'localhost';
+}
+
+const localIP = getLocalIPAddress();
+Config.Url = `http://${localIP}:4047`;
 Config.secret = 'gossip-coco-key';
+
 module.exports = Config;
