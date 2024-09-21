383b183ba075527ff841ea12fbabf44eb73cbfd1
GossipCoco <gossipcoco@hotmail.fr>
2024-09-21 15:22:54 +0200

    dashboard update

--- src/components/Dashboard/Components/AvatarCard.vue
+++ src/components/Dashboard/Components/AvatarCard.vue
@@ -2,15 +2,18 @@
     <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-xs-12 card-global">
         <div class="card display-flex-column profil-card fiction-card">
             <TitleHeaderDashboard v-bind:title="UserName" v-bind:type="'username'"/>
-            <div class="card-body user-info-text">
-                <!-- <p><span v-if="Role"></span>Role : {{ Role.Name }}</p> -->
-                 <p><img :src="'/images/Levels/' + level.Image" />
-
-                 </p>
+            <div class="card-body user-info-text">                
                 <p><span class="label-profil">Inscription</span> <span>{{ formatDate(Inscription) }}</span></p>
                 <p><span class="label-profil">Dernière connextion</span> <span>{{ formatDate(LastConnexion) }}</span></p>
-                <p><span class="label-profil">Vous avez écrit</span> <span>{{ nBFiction }}</span> &nbsp; fictions et &nbsp;<span>{{ totalWords }}</span>&nbsp; mots.</p>                <p><span class="label-profil">Vous avez</span> <span>{{ totalPoints }}</span> points.</p>
-                
+                <p><span class="label-profil">Vous avez écrit</span> <span>{{ nBFiction }}</span> &nbsp; fictions et &nbsp;<span>{{ totalWords }}</span>&nbsp; mots.</p>
+                 <p><span class="label-profil">Vous avez</span> <span>{{ totalPoints }}</span>&nbsp; points.</p>
+                <p><span class="label-profil">Mes badges</span></p>
+                <div class="display-flex-row">
+                    <p>
+                        <img :src="'/images/Levels/' + level.Image" />
+                        <img :src="'/images/Levels/' + roleImage" />
+                    </p>
+                </div>
             </div>
         </div>
     </div>
@@ -20,14 +23,17 @@
 import TitleHeaderDashboard from "../../Components/SpecificComponent/TitleHeaderDashboard.vue";
 export default {
     name: 'AvatarCard',
-    props: ['Avatar', 'UserName', 'LastConnexion', 'Inscription', 'level', 'Role', 'nBFiction', 'totalWords', 'totalPoints'],
+    props: ['Avatar', 'UserName', 'LastConnexion', 'Inscription', 'level', 'Role', 'nBFiction', 'totalWords', 'totalPoints', 'roleImage'],
     components: {TitleHeaderDashboard},
     data() {
         return {
             avatar: this.Avatar,
             lev: this.level,
-            roleUser: this.Role
+            roleUser: this.Role,
         }
+    },
+    created(){
+        // this.roleImage = this.Role.Image
     },
     methods: {
         formatDate(isoDateString) {
