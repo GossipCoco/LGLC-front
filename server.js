159ecb6109664d7f10d37e2e80b623c2465bb72a
GossipCoco <gossipcoco@hotmail.fr>
2024-09-25 19:55:32 +0200

    creation component img + modif server.js

--- server.js
+++ server.js
@@ -1,35 +1,23 @@
 const Config = {};
 
 // Config.Url = "http://192.168.1.10:4047";
-Config.Url = "http://192.168.1.14:4047"
+// Config.Url = "http://192.168.1.14:4047"
 // Config.Url = "http://192.168.1.23:4047";
 
 
 // Config.Url = "http://10.117.60.52:4047"
-// Config.Url = "http://10.117.60.73:   4047"
+// Config.Url = "http://10.117.60.73:4047"
 // Config.Url = "https://192.168.189.223:4047"
 // Config.Url = "http://192.168.168.153:4047"
 // Config.Url = "http://192.168.1.23:4047"
+if (typeof window !== 'undefined') {
+  const hostname = window.location.hostname;
+  const port = '4047'; // Assurez-vous que c'est le port sur lequel votre back-end écoute
+  Config.Url = `http://${hostname}:${port}`;
+} else {
+  // Si vous exécutez du code côté serveur (Node.js), vous pouvez définir une URL par défaut ou utiliser l'adresse IP du serveur
+  Config.Url = 'http://localhost:4047';
+}
 
-// const os = require('os');
-// 
-
-// function getLocalIPAddress() {
-//   const interfaces = os.networkInterfaces();
-//   for (const interfaceName in interfaces) {
-//     for (const iface of interfaces[interfaceName]) {
-//       const { family, internal, address } = iface;
-//       if (family === 'IPv4' && !internal) {
-//         return address;
-//       }
-//     }
-//   }
-//   return 'localhost';
-// }
-
-// const localIP = getLocalIPAddress();
-// console.log(`http://${localIP}:4047`)
-// Config.Url = `http://${localIP}:4047`;
 Config.secret = 'gossip-coco-key';
-
 module.exports = Config;
