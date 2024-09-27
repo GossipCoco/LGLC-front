960aea30f9b8f776f0b37d7910e423ccd6efc6b9
GossipCoco <gossipcoco@hotmail.fr>
2024-09-27 14:16:23 +0200

    chronology up

--- src/components/ArcBook/ArcBookLayout.vue
+++ src/components/ArcBook/ArcBookLayout.vue
@@ -1,14 +1,16 @@
 <template>
   <div
-  id="arc-book-container"
+    id="arc-book-container"
     class="dashboard-max-card-container background-color-main-lineart flex-one card display-flex-column fiction-container"
   >
     <CardHeader v-bind:Title="'Les romans de La Guerre des Clans'" />
     <div class="card-body">
       <div class="row book-container">
         <div class="col-12 col-sm-12 col-md-12">
-          <div v-for="(arc, index) in allBooks" :key="index">
-            <BookLayout v-bind:arc="arc" />           
+          <div class="apland-timeline-area">
+            <div v-for="(arc, index) in allBooks" :key="index">
+              <BookLayout v-bind:arc="arc" />
+            </div>
           </div>
         </div>
       </div>
@@ -33,31 +35,30 @@ export default {
       },
     };
   },
-  created(){
+  created() {
     // this.getAllBooks(this.nav)
-    this.GetAllArcsWithBooks(this.nav)
+    this.GetAllArcsWithBooks(this.nav);
   },
-  methods:{
-    GetAllArcsWithBooks(nav){
-        ArcBookService.GetAllArcsWithBooks(nav)
+  methods: {
+    GetAllArcsWithBooks(nav) {
+      ArcBookService.GetAllArcsWithBooks(nav)
         .then((response) => {
           console.log(response.data.ob);
-          this.allBooks = response.data.ob
+          this.allBooks = response.data.ob;
         })
         .catch((e) => {
           console.log(e);
         });
-    },  
-    getAllBooks(nav){
-        ArcBookService.getAllBooks(nav)
+    },
+    getAllBooks(nav) {
+      ArcBookService.getAllBooks(nav)
         .then((response) => {
           console.log(response.data.ob);
-          
         })
         .catch((e) => {
           console.log(e);
         });
-    }
-  }
+    },
+  },
 };
 </script>
