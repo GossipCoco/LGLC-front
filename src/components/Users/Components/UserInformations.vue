736346da5bc8485c5300fd68cfdae1eb7a1f5672
GossipCoco <gossipcoco@hotmail.fr>
2024-09-30 21:01:51 +0200

    update fiche gamer +  mise en page

--- src/components/Users/Components/UserInformations.vue
+++ src/components/Users/Components/UserInformations.vue
@@ -13,27 +13,38 @@
   </div>
   <h1>Fiche personnage</h1>
   <hr />
-  <div class="all-characters-by-user-container" v-for="(gamer, index) in usrInformation.Gamers" :key="index">
-    <div class="user-information-paragraph">
-      <h3>{{ gamer.UserName }}</h3>
-      <div class="display-flex-row">
-        <div class="display-flex-row">
-          <div class="avatar-gamer-container">
-            <div><img class="avatar-gamer" :src="gamer.Image" /></div>
+  <div class="all-characters-by-user-container">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div
+        class="col"
+        v-for="(gamer, index) in usrInformation.Gamers"
+        :key="index"
+      >
+        <div class="card h-100">
+          <img
+            lass="avatar-gamer"
+            :src="gamer.Image"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h3 class="card-title">{{ gamer.UserName }}</h3>
             <div class="information-character-container">
               <p>Clan de naissance : {{ gamer.ClanId }}</p>
               <p>Statut actuel : {{ gamer.Status }}</p>
               <p>Genre du personnage : {{ gamer.Genre }}</p>
             </div>
           </div>
-          <div class="informations-gamer-container display-flex">
-            <div class="display-flex-column">
-              <h2>Description</h2>
-              <p v-html="gamer.Description"></p>
-              <h2>Personnalité</h2>
-              <p v-html="gamer.Personnality"></p>
-              <h2>Biographie</h2>
-              <p v-html="gamer.Biography"></p>
+          <div class="card-footer">
+            <div class="informations-gamer-container display-flex">
+              <div class="display-flex-column">
+                <h2>Description</h2>
+                <p v-html="gamer.Description"></p>
+                <h2>Personnalité</h2>
+                <p v-html="gamer.Personnality"></p>
+                <h2>Biographie</h2>
+                <p v-html="gamer.Biography"></p>
+              </div>
             </div>
           </div>
         </div>
