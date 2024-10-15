f9c29425926b35d556f2c9de8da7d4e57eaff54c
GossipCoco <gossipcoco@hotmail.fr>
2024-10-15 21:11:27 +0200

    up card clan details

--- src/components/Locations/ClanDetails.vue
+++ src/components/Locations/ClanDetails.vue
@@ -2,14 +2,14 @@
   <div class="card card-image-layout clan-detail-layout display-flex-row mb-3">
     <div class="row g-0">
       <cardImageBackground v-bind:Image="Image"/>
-      <div class="col-8 col-md-8 flex-one">
-        <div class="card-body white-text background-color-main-lineart details-infos-characters">
-          <h5 class="card-header">{{ clan.Name }}</h5>
-          <div class="card-text">
-            <div v-html="clan.Description"></div>
-            <div class="display-flex-row flex-one align-items-content-justify-content all-characters-list-containers">
-              <CharactersListe v-bind:characters="clan.Warriors" />
-            </div>
+      <div class="col-8 col-md-8 flex-one background-color-main-lineart">
+        <CardHeader v-bind:Title="clan.Name"><img :src="'/images/clans/' + clan.Symbol"/></CardHeader>
+          <div class="card-body white-text background-color-main-lineart details-infos-characters">
+            <div class="card-text">
+              <div v-html="clan.Description"></div>
+              <div class="display-flex-row flex-one align-items-content-justify-content all-characters-list-containers">
+                <CharactersListe v-bind:characters="clan.Warriors" />
+              </div>
           </div>
         </div>
       </div>
@@ -20,9 +20,10 @@
 import ClanService from "../../services/ClansServices";
 import CharactersListe from "../Components/GenericComponent/CharactersListe.vue";
 import cardImageBackground from "../Components/GenericComponent/cardImageBackground.vue";
+import CardHeader from "../Components/GenericComponent/CardHeader.vue"
 export default {
   name: "ClanDetails",
-  components: { CharactersListe, cardImageBackground },
+  components: { CharactersListe, cardImageBackground, CardHeader },
   data() {
     return {
       url: "",
