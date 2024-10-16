ee852e989dd2febee03df070f0ef2ff65dce730a
GossipCoco <gossipcoco@hotmail.fr>
2024-10-16 08:31:38 +0200

    character clan

--- src/components/Components/GenericComponent/CardHeader.vue
+++ src/components/Components/GenericComponent/CardHeader.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="display-flex card-header display-flex-row">
-    <h2>{{ Title }}</h2>
-    <slot></slot>
+    <div class="row">
+      <div class="col-6">
+        <h2>{{ Title }}</h2>
+      </div>
+      <div class="col-6">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-export default{
-    name: 'CardHeader',
-    props: ['Title']
-}
+export default {
+  name: "CardHeader",
+  props: ["Title"],
+};
 </script>
