a474ba8c4672f1234a69265e5b4b7e3b6b6200b2
GossipCoco <gossipcoco@hotmail.fr>
2024-10-17 08:01:06 +0200

    up image characters border radius

--- src/components/Dashboard/Components/TitleHeader.vue
+++ src/components/Dashboard/Components/TitleHeader.vue
@@ -2,10 +2,10 @@
     <div class="card">
         <div class="card-body display-flex-row align-items-content-justify-content  title-dashboard-container-header">
             <h1>Bienvenue sur votre tableau de bord</h1>
-             <ImageRondDashboard v-bind:src="'/images/Avatars/' + Avatar" v-bind:alt="Avatar" v-bind:classe="'card-img-top'"/>
+             <ImageRondDashboard v-bind:src="'/images/Avatars/' + Avatar" v-bind:alt="Avatar" v-bind:classe="'card-img-top mini-rond-avatar-image'"/>
              <div class="image-messenger-container">
                 <router-link :to="'/user/messagerie/'+User" class="display-flex-row message-sent-container" >
-                    <img src="/images/icons/envelope-solid.svg" />
+                    <img src="/images/icons/envelope-solid.svg"/>
                     <div class="mini-rond-messages"><p>{{ NbMessages }}</p></div>
                 </router-link>
             </div>
