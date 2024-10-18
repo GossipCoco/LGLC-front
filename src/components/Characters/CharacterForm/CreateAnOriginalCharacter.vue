45608700d3d69574e74041e6d60834f434ec7fe1
GossipCoco <gossipcoco@hotmail.fr>
2024-10-18 13:21:22 +0200

    up refractor css fix height form

--- src/components/Characters/CharacterForm/CreateAnOriginalCharacter.vue
+++ src/components/Characters/CharacterForm/CreateAnOriginalCharacter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="display-flex-column form-character-container flex-one opensans-text">
-    <div class="card border-none white-text display-flex-column card-form-create-character align-items-content-justify-content flex-one mb-12">
+    <div class="card border-none background-color-main-lineart overflowY-X-hidden white-text display-flex-column card-form-create-character align-items-content-justify-content flex-one mb-12">
       <CardHeader :Title="title" style="width: 100%;"/>
       <OriginalCharacterForm />
     </div>
