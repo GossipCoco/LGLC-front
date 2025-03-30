71a79d7b886324fc753e73b5ad028de0d3619fc5
GossipCoco <gossipcoco@hotmail.fr>
2025-03-30 15:37:47 +0200

    update image avatar dasboard

--- src/components/Dashboard/Components/TitleHeader.vue
+++ src/components/Dashboard/Components/TitleHeader.vue
@@ -2,7 +2,7 @@
     <div class="card background-lineart border-none">
         <div class="card-body display-flex-row align-items-content-justify-content title-dashboard-container-header">
             <h1 class=" font-variant-small-caps cinzel text-white">Bienvenue sur votre tableau de bord {{ User }}</h1>
-             <ImageRondDashboard v-bind:src="'/images/Avatars/' + Avatar" v-bind:alt="Avatar" v-bind:classe="'card-img-top mini-rond-avatar-image'"/>
+             <ImageRondDashboard v-bind:src="'/images/Avatars/' + Avatar" v-bind:alt="Avatar" v-bind:classe="'card-img-top mini-rond-avatar-image'" v-bind:edit="true"/>
              <div class="image-messenger-container">
                 <router-link :to="'/user/messagerie/'+User" class="display-flex-row message-sent-container" >
                     <img src="/images/icons/envelope-solid.svg"/>
