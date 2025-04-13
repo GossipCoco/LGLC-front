d7ee96dac4da0faa703b2fc0f5b4c55309ac287a
GossipCoco <gossipcoco@hotmail.fr>
2025-04-13 14:44:54 +0200

    update css

--- src/components/Characters/CharacterComponent/OriginaleCharacterByUser.vue
+++ src/components/Characters/CharacterComponent/OriginaleCharacterByUser.vue
@@ -1,11 +1,46 @@
 <template>
-  <div id="original-characters-list" class="row row-cols-1 row-cols-md-2 g-4 all-my-characters-container 
-  display-flex align-items-content-justify-content overflowY-X-hidden">
-    <div class="col card-character-container height-20-rem" v-for="(character, index) in allMyCharacters" :key="index">
+  <div class="row row-filter background-color-dark-green-01 display-flex vertical-align-middle">
+    <InputName
+      class="input-name-margin"
+      v-bind:col="'col-xxl-4 col-xl-3 col-lg-12 col-md-12 col-sm-12 col-xs-12'"
+      v-bind:forId="'inputName'"
+      v-bind:label="'Nom du personnage'"
+      v-bind:getNameData="'getCurrentName'"
+      @getCurrentName="getCurrentName"
+    />
+    <GenericSelect
+      v-bind:col="'col-xxl-3 col-xl-3 col-lg-12 col-md-12 col-sm-12 col-xs-12'"
+      v-bind:For="'SearchClan'"
+      v-bind:label="'Clan'"
+      v-bind:formdata="'getclans'"
+      v-bind:clans="clans"
+      @getclans="getclans"
+    />
+    <GenericSelect
+      v-bind:col="'col-xxl-3 col-xl-3 col-lg-12 col-md-12 col-sm-12 col-xs-12'"
+      v-bind:For="'SearchGrade'"
+      v-bind:label="'Grade'"
+      v-bind:formdata="'getgrade'"
+      @getgrade="getgrades"
+    />
+  </div>
+  <div
+    id="original-characters-list"
+    class="row row-cols-1 row-cols-md-2 g-4 all-my-characters-container display-flex align-items-content-justify-content overflowY-X-hidden"
+  >
+    <div
+      class="col card-character-container height-20-rem"
+      v-for="(character, index) in allMyCharacters"
+      :key="index"
+    >
       <div class="card-group display-flex">
         <div class="card individual-character-card">
           <BackgroundImage v-bind:Image="character.Image" />
-          <CharacterCardBody v-bind:id="character.Id" v-bind:name="character.CurrentName" v-bind:route="'/OriginaleCharacterDetails/'"/>
+          <CharacterCardBody
+            v-bind:id="character.Id"
+            v-bind:name="character.CurrentName"
+            v-bind:route="'/OriginaleCharacterDetails/'"
+          />
         </div>
       </div>
     </div>
@@ -28,15 +63,25 @@ import CharacterService from "../../../services/CharacterService";
 import Pagination from "../../Components/GenericComponent/Pagination.vue";
 import BackgroundImage from "./BackgroundImage.vue";
 import CharacterCardBody from "./CharacterCardBody.vue";
+import InputName from "../../Components/FormComponent/InputName.vue";
+import GenericSelect from "../../Components/FormComponent/GenericSelect.vue";
+
 export default {
   name: "OriginaleCharacterByUser",
-  components: { Pagination, BackgroundImage, CharacterCardBody },
+  components: {
+    Pagination,
+    BackgroundImage,
+    CharacterCardBody,
+    InputName,
+    GenericSelect,
+  },
   data() {
     return {
       usr: this.$store.state.auth.user.usrID,
       NbAllCharacters: null,
       allMyCharacters: {},
       filters: [],
+      width: 1750,
       nav: {
         current: 0,
         pages: 0,
@@ -74,6 +119,13 @@ export default {
       this.GetOriginaleCharacterByUser(this.usr, this.nav);
     },
     GetOriginaleCharacterByUser(user, nav) {
+      if(window.innerWidth >= this.width){
+          this.nav.step = 10
+        }else if(window.innerWidth < this.width)
+        {
+          this.nav.step = 8
+        }
+
       CharacterService.GetOriginaleCharacterByUser(user, nav)
         .then((response) => {
           this.allMyCharacters = response.data.ob;
