cdd200ee359fc0d149b108130f87f2e7b7314aa9
GossipCoco <gossipcoco@hotmail.fr>
2025-04-16 07:23:17 +0200

    update book

--- src/components/Components/GenericComponent/CardHeader.vue
+++ src/components/Components/GenericComponent/CardHeader.vue
@@ -4,10 +4,10 @@
   display-flex card-header display-flex-row flex-one align-items-content-justify-content text-white cinzel"
   >
     <div class="row" style="width: 100%;">
-      <div class="col-10">
+      <div class="col-8">
         <h2 class="display-flex flex-one align-items-content-justify-content text-white">{{ Title }}</h2>
       </div>
-      <div class="col-2">
+      <div class="col-4">
         <slot></slot>
       </div>
     </div>
