a435dfd9152578d6dc1b8e8d79206e298e619df0
GossipCoco <gossipcoco@hotmail.fr>
2025-04-24 21:09:45 +0200

    fix and update profil user

--- src/components/Users/Components/OCGamerCard.vue
+++ src/components/Users/Components/OCGamerCard.vue
@@ -32,5 +32,6 @@
 export default {
   name: "OCGamerCard",
   props: ["gamers"],
+  
 };
 </script>
--- src/components/Users/Components/UserInformations.vue
+++ src/components/Users/Components/UserInformations.vue
@@ -1,28 +1,57 @@
 <template>
-  <div class="card-body user-information-container">
+  <div v-if="!showspinner" class="card-body user-information-container">
     <div class="padding-2-rem white-text">
       <div class="display-flex-row">
         <div class="row">
           <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 col-xxl-2">
-        <ImageRondDashboard v-bind:src="'/images/Avatars/' + usrInformation.Avatar" v-bind:alt="usrInformation.Avatar" v-bind:classe="'card-img-top mini-rond-avatar-image'"/>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 col-xxl-10">
-        <div class="display-flex-column">
-          <p class="roboto roboto">
-            {{ usrInformation.LastName }} {{ usrInformation.FirstName }} -
-            {{ usrInformation.UserName }}
-          </p>
-          <p class="roboto roboto">{{ usrInformation.Description }}</p>
-          <p class="roboto roboto">
-            {{ usrInformation.Biography }} 
-            
-          </p>
-        </div>
-        </div>
+            <ImageRondDashboard
+              v-bind:src="'/images/Avatars/' + usrInformation.Avatar"
+              v-bind:alt="usrInformation.Avatar"
+              v-bind:classe="'card-img-top mini-rond-avatar-image'"
+            />
+          </div>
+          <div
+            class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 col-xxl-10"
+          >
+            <div class="display-flex-column user-profil-image">
+              <div class="display-flex-row">
+                <div class="display-flex-column user-profil-image">
+                  <p class="text-white poppins-text">
+                    Rôle : {{ usrInformation.Level.Name }}
+                  </p>
+                  <img
+                    :src="'/images/Levels/' + usrInformation.Level.Image"
+                    :alt="usrInformation.Level.Image"
+                    loading="lazy"
+                    class="level-role-width"
+                  />
+                </div>
+                <div class="display-flex-column user-profil-image">
+                  <p class="text-white poppins-text">
+                    Niveau : {{ usrInformation.Role.Name }}
+                  </p>
+                  <img
+                    :src="'/images/Levels/' + usrInformation.Role.Image"
+                    :alt="usrInformation.Role.Image"
+                    loading="lazy"
+                    class="level-role-width"
+                  />
+                </div>
+              </div>
+              <p class="roboto roboto">
+                {{ usrInformation.LastName }} {{ usrInformation.FirstName }} -
+                {{ usrInformation.UserName }}
+              </p>
+              <p class="roboto roboto">{{ usrInformation.Description }}</p>
+              <p class="roboto roboto">
+                {{ usrInformation.Biography }}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <OCGamerCard v-bind:gamers="usrInformation.Gamers" />
+    <OCGamerCard v-bind:gamers="usrInformation.Gamers" v-if="!showspinner" />
   </div>
 </template>
 <script>
@@ -31,10 +60,11 @@ import ImageRondDashboard from "../../Components/SpecificComponent/ImageRondDash
 import OCGamerCard from "./OCGamerCard.vue";
 export default {
   name: "UserInformations",
-  components:{ImageRondDashboard, OCGamerCard},
+  components: { ImageRondDashboard, OCGamerCard },
   props: ["user"],
   data() {
     return {
+      showspinner: false,
       usr: this.user,
       usrInformation: {},
     };
