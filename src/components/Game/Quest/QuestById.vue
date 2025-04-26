ea379e8bec782d4e58d6f74664676e61dcc10563
GossipCoco <gossipcoco@hotmail.fr>
2025-04-27 00:02:17 +0200

    fix and update

--- src/components/Game/Quest/QuestById.vue
+++ src/components/Game/Quest/QuestById.vue
@@ -1,12 +1,33 @@
 <template>
-  <div id="quest-by-id" class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 card-global flex-wrap quest-container flex-one width-100-percent">
-    <div class="card quest-by-id-card background-color-main-lineart width-100-percent">
-      <card-header v-bind:Title="quest.Title"><router-link class="btn btn-primary" to="/Quest">Retour à la liste de quêtes</router-link></card-header> />
+  <div
+    id="quest-by-id"
+    class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 card-global flex-wrap quest-container flex-one width-100-percent"
+  >
+    <div
+      class="card quest-by-id-card background-color-main-lineart width-100-percent"
+    >
+      <card-header v-bind:Title="quest.Title"
+        ><router-link class="btn btn-primary" to="/Quest"
+          >Retour à la liste de quêtes</router-link
+        ></card-header
+      >
+      />
       <div class="card-body">
+        <div v-for="(parallax, index) in layers" :key="index">
+          
+          <img :src="parallax.Image"/> 
+          <!-- {{ parallax.Image }} -->
+        </div>
         <div class="parallax-container width-100-percent">
           <QuestMissionTarget v-bind:questDatas="quest" />
-          <QuestQuestion v-if="quest.TypeQuest === 'question'" v-bind:questData="quest" />
-          <QuestKeyObjects v-if="quest.TypeQuest === 'object'" v-bind:findobjects="quest.QuestKeyObjects"/>
+          <QuestQuestion
+            v-if="quest.TypeQuest === 'question'"
+            v-bind:questData="quest"
+          />
+          <QuestKeyObjects
+            v-if="quest.TypeQuest === 'object'"
+            v-bind:findobjects="quest.QuestKeyObjects"
+          />
           <QuestLayers v-bind:layersData="layers" />
         </div>
       </div>
@@ -24,7 +45,13 @@ import QuestKeyObjects from "./Components/QuestKeyObjects.vue";
 
 export default {
   name: "QuestById",
-  components: { CardHeader, QuestQuestion, QuestMissionTarget, QuestLayers, QuestKeyObjects },
+  components: {
+    CardHeader,
+    QuestQuestion,
+    QuestMissionTarget,
+    QuestLayers,
+    QuestKeyObjects,
+  },
   data() {
     return {
       quest: {},
@@ -49,16 +76,24 @@ export default {
     GetQuestById(id) {
       QuestService.GetQuestById(id)
         .then((response) => {
-          console.log(response.data.ob);
+          console.log(response.data.ob.QuestParallaxes);
           this.quest = response.data.ob;
-          this.layers = response.data.ob.QuestParallaxes.map((item) => ({
-            Image: `/images/parallax/${item.Parallax.Image}`,
-            Position: item.Parallax.Position,
-            translateZ: item.Parallax.translateZ,
-            translateY: item.Parallax.translateY,
-            translateX: item.Parallax.translateX,
-            scale: item.Parallax.scale,
-          }));
+          this.layers = response.data.ob.QuestParallaxes.map((item) => {
+            console.log(item.Parallax)
+            if (!item.Parallax) {
+              console.warn("Pas de Parallax pour QuestParallax id:", item.Id);
+              return null; // ou {} vide
+            }
+            return {
+              Image: `/images/parallax/${item.Parallax.Image}`,
+              Position: item.Parallax.Position,
+              translateZ: item.Parallax.translateZ,
+              translateY: item.Parallax.translateY,
+              translateX: item.Parallax.translateX,
+              scale: item.Parallax.scale,
+            };
+          }).filter((item) => item !== null);
+          console.table(this.layers);
         })
         .catch((err) => {
           console.log(err);
