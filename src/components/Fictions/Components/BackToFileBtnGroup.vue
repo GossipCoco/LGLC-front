7cfd417860f7475e6ef5673cf1d7bd44d023a380
GossipCoco <gossipcoco@hotmail.fr>
2025-04-26 15:42:13 +0200

    fix and update components.css

--- src/components/Fictions/Components/BackToFileBtnGroup.vue
+++ src/components/Fictions/Components/BackToFileBtnGroup.vue
@@ -1,25 +1,26 @@
 <template>
-          <div class="back-to-all-fictions-container">
-        <div class="flex-one display-flex-column align-items-content-justify-content">
-          <router-link
-            type="button"
-            class="btn btn-primary"
-            :to="'/allFictions/' + Author"
-            >Retour à la liste mes fictions</router-link
-          >
-          <router-link
-            type="button"
-            class="btn btn-primary"
-            :to="'/AllExistingFictionsLayout'"
-            >Retour à la liste des fictions</router-link
-          >
-        </div>
-      </div>
+  <div class="back-to-all-fictions-container">
+    <div
+      class="flex-one display-flex-column align-items-content-justify-content"
+    >
+      <router-link
+        type="button"
+        class="btn btn-primary margin-1-0-0-0"
+        :to="'/allFictions/' + Author"
+        >Retour à la liste mes fictions</router-link
+      >
+      <router-link
+        type="button"
+        class="btn btn-primary margin-1-0-0-0"
+        :to="'/AllExistingFictionsLayout'"
+        >Retour à la liste des fictions</router-link
+      >
+    </div>
+  </div>
 </template>
 <script>
-
 export default {
-    name: 'BackToFileBtnGroup',
-    props:['Author']
-}
+  name: "BackToFileBtnGroup",
+  props: ["Author"],
+};
 </script>
