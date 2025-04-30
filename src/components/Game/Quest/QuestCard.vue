bda122fe589802edb84aec2c08f106c5a23c7248
GossipCoco <gossipcoco@hotmail.fr>
2025-04-30 20:45:34 +0200

    fix and update css quest

--- src/components/Game/Quest/QuestCard.vue
+++ src/components/Game/Quest/QuestCard.vue
@@ -7,8 +7,7 @@
     <div class="background-image-quest width-10-vh height-16-rem background-size-cover flex-one" v-bind:style="{ backgroundImage: 'url(' + image.Image + ')',}"></div>
     <div class="card-body display-flex-column one-quest flex-one width-10-rem">
       <div class="display-flex-column quest-button-text height-30-vh align-items-content-justify-content vertical-align-middle">
-        <!-- <h6 class="width-5-vh">{{ quest.Title }}</h6> -->
-        <p class="display-flex-column quest-description text-white display-none-responsive">{{ quest.Description }}</p>
+        <p class="display-flex-column quest-description font-weight-400 text-white display-none-responsive">{{ quest.Description }}</p>
         <p class="display-flex-column quest-button-link">
           <router-link
             :to="'/QuestById/' + quest.Id"
