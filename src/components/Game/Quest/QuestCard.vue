e9c66e9ee087e84fac63a4d968e437e670640683
GossipCoco <gossipcoco@hotmail.fr>
2025-04-30 08:37:36 +0200

    fix, refracto  and update refracto fiction contain

--- src/components/Game/Quest/QuestCard.vue
+++ src/components/Game/Quest/QuestCard.vue
@@ -6,7 +6,7 @@
   <div class="display-flex-row flex-one ">
     <div class="background-image-quest width-10-vh height-16-rem background-size-cover flex-one" v-bind:style="{ backgroundImage: 'url(' + image.Image + ')',}"></div>
     <div class="card-body display-flex-column one-quest flex-one width-10-rem">
-      <div class="display-flex-column quest-button-text align-items-content-justify-content vertical-align-middle">
+      <div class="display-flex-column quest-button-text height-30-vh align-items-content-justify-content vertical-align-middle">
         <!-- <h6 class="width-5-vh">{{ quest.Title }}</h6> -->
         <p class="display-flex-column quest-description text-white display-none-responsive">{{ quest.Description }}</p>
         <p class="display-flex-column quest-button-link">
