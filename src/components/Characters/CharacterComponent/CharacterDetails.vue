3e2c7c64f6a06db62c9c1f11ec8c387ecf0adba7
GossipCoco <gossipcoco@hotmail.fr>
2025-05-01 08:29:17 +0200

    fix and update rfracto

--- src/components/Characters/CharacterComponent/CharacterDetails.vue
+++ src/components/Characters/CharacterComponent/CharacterDetails.vue
@@ -3,7 +3,7 @@
     <div class="row g-0">
       <CharacterImageDetail v-bind:Image="background">
         <img
-            class="height-auto display-block width-100-percent"
+            class="height-auto display-block width-100-percent overflow-hidden"
             :src="image"
             :alt="image"
           />
