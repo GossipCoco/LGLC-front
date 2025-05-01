68929a503d7914262408081bb218da3002c4458f
GossipCoco <gossipcoco@hotmail.fr>
2025-05-01 21:51:29 +0200

    fix and update refracto

--- src/components/Components/GenericComponent/CardHeader.vue
+++ src/components/Components/GenericComponent/CardHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <div
-  class="
-  display-flex card-header display-flex-row flex-one align-items-content-justify-content text-white cinzel"
+    class="display-flex card-header display-flex-row flex-one
+    align-items-content-justify-content text-white cinzel"
   >
     <div class="row width-190-vh">
       <div class="col-8">
-        <h2 class="display-flex flex-one align-items-content-justify-content text-white width-180-vh">{{ Title }}</h2>
+        <h2 class="display-flex flex-one align-items-content-justify-content text-white width-180-vh">
+          {{ Title }}
+        </h2>
       </div>
       <div class="col-4">
         <slot></slot>
