e5b8f42904d6c8d382f0cfeeaf061e5a3f4b8195
GossipCoco <gossipcoco@hotmail.fr>
2025-05-01 14:37:53 +0200

    fix and update rfracto + chapter css

--- src/components/Fictions/AllMyFictions/ListOfChapters.vue
+++ src/components/Fictions/AllMyFictions/ListOfChapters.vue
@@ -2,10 +2,11 @@
   <div class="opacity-container height-80 display-flex-row">
     <div class="summary-container height-70-vh overflow-hidden">
       <div class="row top-fiction" v-if="AuthorId === usrCurrent">        
-        <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2 col-xs-2">
+        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
           <div v-if="nbKind > 0 " class="display-flex-row kind-fiction-container">
+            Genre(s) : &nbsp;
             <p v-for="(kind, index) in Kinds" :key="index" class="display-flex-row">
-              <span>{{ kind.KindId }}</span>&nbsp;-&nbsp; 
+              <span>{{ kind.KindId }}</span><span v-if="index < Kinds.length - 1">&nbsp;-&nbsp;</span>
             </p>
           </div>
         </div>
@@ -13,17 +14,17 @@
           <EditSummary v-bind:FictionId="IdFiction" v-bind:Summary="Summary" v-if="AuthorId === usrCurrent"/>
         </div>
         <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3 upload-input-component">
-           <UploadImage
+           <!-- <UploadImage
             v-bind:id="IdFiction"
             v-bind:service="'UploadFictionBackgroundIllustration'"
             v-bind:css="'mb-3'"
             v-if="AuthorId === usrCurrent"
-          />
+          /> -->
         </div>
-        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3 btn-create-chapter-container">
             <router-link
               type="button"
-              class="btn btn-primary btn-lg"
+              class="btn btn-primary btn-lg btn-create-chapter-button"
               v-if="AuthorId === usrCurrent"
               :to="'/fiction/createChapter/' + IdFiction"
               v-bind="lastChap"
@@ -64,7 +65,7 @@
 import EditSummary from "../FictionsForm/EditSummary.vue";
 import AddANewCharacterModal from "./AddANewCharacterModal.vue";
 import CarrouselCharacter from "./CarrouselCharacter.vue";
-import UploadImage from "../../Components/GenericComponent/UploadImage.vue";
+// import UploadImage from "../../Components/GenericComponent/UploadImage.vue";
 import ListAllChapters from "../Components/ListAllChapters.vue";
 export default {
   name: "ListOfChapters",
@@ -87,7 +88,7 @@ export default {
     EditSummary,
     CarrouselCharacter,
     AddANewCharacterModal,
-    UploadImage,
+    // UploadImage,
     ListAllChapters,
   },
   data() {
