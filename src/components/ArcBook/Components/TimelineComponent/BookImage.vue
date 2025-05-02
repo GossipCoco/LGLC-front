e8efeaf5cffa8f01c71c5ee973a30e4037a9521b
GossipCoco <gossipcoco@hotmail.fr>
2025-05-02 20:46:07 +0200

    fix and update refracto fix timeline add img

--- src/components/ArcBook/Components/TimelineComponent/BookImage.vue
+++ src/components/ArcBook/Components/TimelineComponent/BookImage.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="col-2 col-sm-12 col-md-2 col-lg-2 col-xxl-2 image-book-container display-flex-row align-items-content-justify-content">
-        <img :src="Illustration" :alt="Title" class="image-book height-30-vh" />
-    </div>k
+    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xxl-2 image-book-container display-flex-row align-items-content-justify-content">
+        <img :src="Illustration" :alt="Title" class="image-book height-20-vh width-20-vh" />
+    </div>
 </template>
 <script>
     export default{
