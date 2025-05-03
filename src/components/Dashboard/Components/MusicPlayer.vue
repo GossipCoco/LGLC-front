383b1ee1423f87ec166fd495de638f7889bacefa
GossipCoco <gossipcoco@hotmail.fr>
2025-05-03 15:01:32 +0200

    fix and update refracto

--- src/components/Dashboard/Components/MusicPlayer.vue
+++ src/components/Dashboard/Components/MusicPlayer.vue
@@ -2,7 +2,7 @@
   <div class="col-xxl-3 col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-12 card-global">
     <div class="card background-lineart border-none fiction-card scheduler">
         <TitleHeaderDashboard v-bind:title="'Musique'" v-bind:type="'fictions'"/>
-        <div class="card-body scheduler-container">
+        <div class="card-body">
       <div class="music-player">
         <audio ref="audioPlayer" controls @ended="nextTrack" width="14rem">
           <source :src="currentTrack.url" type="audio/mpeg" />
