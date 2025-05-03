a1aa23b27c7640cf5c87fa1662e5f24baa203f71
GossipCoco <gossipcoco@hotmail.fr>
2025-05-03 21:23:12 +0200

    fix and update refracto ux info character original

--- src/components/Characters/GenericComponent/CharacterImageDetail.vue
+++ src/components/Characters/GenericComponent/CharacterImageDetail.vue
@@ -2,7 +2,7 @@
   <div class="col-4 col-md-4">
     <div class="character-details-card background-size-cover display-flex background-size-cover
     card-image flex-one border-card-color display-flex-row align-items-content-justify-content
-    image-illustration-right original-character-contain"
+    image-illustration-right original-character-contain height-90-vh width-80-vh"
       v-bind:style="{
         backgroundImage: 'url(' + Image + ')',
       }"
