94586f3c2db84668b278111f29cbbe84c3298c28
GossipCoco <gossipcoco@hotmail.fr>
2025-05-04 09:36:28 +0200

    fix and update refracto ux  css border

--- src/components/Fictions/Components/IllustrationSummaryCol.vue
+++ src/components/Fictions/Components/IllustrationSummaryCol.vue
@@ -2,13 +2,13 @@
     <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12 padding-0-0-0-5vh col-left">
         <div class="display-flex-column">
         <div
-            class="display-flex-column image-fiction-container margin-21vh-0-0-0 all-illustrations-fiction-container padding-2-vh overflowY-X-hidden background-size-cover"
+            class="display-flex-column border-radius-12px image-fiction-container margin-21vh-0-0-0 all-illustrations-fiction-container padding-2-vh overflowY-X-hidden background-size-cover"
             v-bind:style="{
                 backgroundImage: 'url(' + backgroundImageFiction + ')',
             }"
             >
         </div>
-        <div class="summary-fiction-container-wrapper width-50-vh height-35-vh overflowY-scroll margin-1vh-0-0-1vh">
+        <div class="summary-fiction-container-wrapper border-radius-12px width-50-vh height-35-vh overflowY-scroll margin-1vh-0-0-1vh">
             <p class="text-align-justify overflowY-auto summary-height-container Noto-Sans" v-html="Summary"></p>
         </div>
         </div>
