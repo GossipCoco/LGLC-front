785086aa2f59d66eccd78e25994deb0240de55cf
GossipCoco <gossipcoco@hotmail.fr>
2025-05-05 20:06:21 +0200

    fix request refracto fix component Arc add elemen

--- src/components/ArcBook/Components/OneArc.vue
+++ src/components/ArcBook/Components/OneArc.vue
@@ -1,17 +1,27 @@
 <template>
-    <div class="one-arc-container-wrapper margin-2vh-0">
+    <div class="one-book-container-wrapper one-arc-container-wrapper margin-2vh-0">
         <div class="display-flex-row flex-one">
+            <card-image-background v-bind:Image="background">                
+            </card-image-background>
+        </div>
+        <div id="arc-container" class="border-none background-color-main-lineart flex-one card display-flex-column height-90-vh padding-2-vh">
 
         </div>
     </div>
 </template>
 <script>
 import ArcBookService from '../../../services/ArcBookService';
+
+import cardImageBackground from "../../Components/GenericComponent/cardImageBackground.vue";
 export default {
     name: 'OneArc',
+    components:{
+        cardImageBackground
+    },
     data(){
         return{
             url: null,
+            background: null,
             arc:{}
         }
     },
@@ -24,6 +34,8 @@ export default {
             ArcBookService.GetArcByName(id)
             .then((response) => {
                 console.log(response.data.ob)
+                this.arc = response.data.ob
+                this.background = response.data.ob.IllustrationOne
             })            
             .catch((e) => {
             console.log(e);
