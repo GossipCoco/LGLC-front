ee42ea8261331f1edb967502a303b1a4deaa074c
GossipCoco <gossipcoco@hotmail.fr>
2025-05-06 20:51:07 +0200

    fix request refracto cards height width character

--- src/components/Characters/GenericComponent/BackgroundImage.vue
+++ src/components/Characters/GenericComponent/BackgroundImage.vue
@@ -3,7 +3,7 @@
     class="
     character-details-card background-size-cover display-flex background-size-cover
     card-image background-size-cover display-flex-row align-items-content-justify-content
-    flex-one border-card-color image-illustration-right background-position-50-percent height-30-vh"
+    flex-one border-card-color image-illustration-right background-position-50-percent height-35-vh"
     v-bind:style="{ backgroundImage: 'url(' + Image + ')',}"
     >
         <slot></slot>
