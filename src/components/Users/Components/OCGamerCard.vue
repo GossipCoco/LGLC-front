b98ab7fcd3d91338572a3a8a16a7948f7250aa60
GossipCoco <gossipcoco@hotmail.fr>
2025-05-06 16:11:08 +0200

    fix request refracto usercard profil oc card

--- src/components/Users/Components/OCGamerCard.vue
+++ src/components/Users/Components/OCGamerCard.vue
@@ -4,10 +4,10 @@
   <hr />
   <div class="all-characters-by-user-container background-color-main-lineart height-75-vh overflowY-scroll overflowX-hidden">
     <div class="row row-cols-1 row-cols-md-4 g-4">
-      <div class="col-3 background-color-main-lineart" v-for="(gamer, index) in gamers" :key="index">        
+      <div class="col-3" v-for="(gamer, index) in gamers" :key="index">        
           <div class="display-flex width-40-vh align-items-content-justify-content">
-            <div class="card background-color-main-lineart display-flex align-items-content-justify-content">
-              <div class="card-header">
+            <div class="card border-none background-color-main-lineart display-flex align-items-content-justify-content">
+              <div class="card-header width-40-vh">
                 <h3 class="card-title cinzel margin-2vh-0-0-0">
                   <router-link :to="'/OriginaleCharacterDetails/' + gamer.CurrentName" class="text-white">
                     {{ gamer.CurrentName }}
