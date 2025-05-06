0699419b4c20f14c45c80d0237c1ba9d8bf6974a
GossipCoco <gossipcoco@hotmail.fr>
2025-05-06 07:59:22 +0200

    fix request refracto arc one mise en page et affichage des données

--- src/components/ArcBook/Components/OneArc.vue
+++ src/components/ArcBook/Components/OneArc.vue
@@ -8,15 +8,21 @@
             <CardHeader v-bind:Title="arc.Title">
                 <router-link to="/ArcBookLayout" class="btn btn-primary">Retour à la liste des arcs</router-link>
             </CardHeader>
-            <div class="card-body width-120-vh height-80-vh padding-2-vh margin-2vh-0">
+            <div class="card-body width-130-vh height-80-vh padding-2-vh margin-2vh-0">
                 <div class="row">
                     <div class="col-12">
                         <p class="white-text">Résumé : </p>
-                        <p class="white-text" v-html="arc.Summary"></p>
-                        <div v-for="(books, index) in arc.Books" :key="index">
-                            <p  class="white-text">{{ books.Title }}</p>
+                        <p class="white-text text-align-justify" v-html="arc.Summary"></p>
+                        <div  class="display-flex-row">
+                            <div v-for="(books, index) in arc.Books" :key="index">
+                                <p class="white-text display-flex-column">
+                                    <img class="image-book border-radius-2vh-0-0-2vh height-20-vh" :src="books.Image" />
+                                    <router-link class="white-text" :to="'/BookByTitle/' + books.Title">
+                                        {{ books.Title }}
+                                    </router-link>
+                                </p>
+                            </div>
                         </div>
-                        
                     </div>
                 </div>
             </div>
