f420cc4bd7d079802477fab78e0d31b27f684535
GossipCoco <gossipcoco@hotmail.fr>
2025-05-09 15:20:03 +0200

    fix request refracto avatar card

--- src/components/Dashboard/Components/AvatarCard.vue
+++ src/components/Dashboard/Components/AvatarCard.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 card-global">
-
       <div class="card height-60-vh padding-0-rem border-none display-flex-column profil-card fiction-card background-lineart">
         <TitleHeaderDashboard
           v-bind:title="UserName"
@@ -39,7 +38,6 @@
           />
         </div>
       </div>
-
   </div>
 </template>
 <script>
