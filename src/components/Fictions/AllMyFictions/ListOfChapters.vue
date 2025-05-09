b5a38b0ab080819b365cea450d63b8bc44e558af
GossipCoco <gossipcoco@hotmail.fr>
2025-05-09 12:48:41 +0200

    fix request refracto all fictions

--- src/components/Fictions/AllMyFictions/ListOfChapters.vue
+++ src/components/Fictions/AllMyFictions/ListOfChapters.vue
@@ -78,7 +78,6 @@ import EditSummary from "../FictionsForm/EditSummary.vue";
 import AddANewCharacterModal from "../Components/AddANewCharacterModal.vue";
 import AddAOriginalCharacterModal from "../Components/AddANewCharacterModal.vue";
 import CarrouselCharacter from "../Components/CarrouselCharacter.vue";
-// import UploadImage from "../../Components/GenericComponent/UploadImage.vue";
 import ListAllChapters from "../Components/ListAllChapters.vue";
 import CreateChapterButton from '../Components/CreateChapterButton.vue';
 import IllustrationSummaryCol from '../Components/IllustrationSummaryCol.vue';
