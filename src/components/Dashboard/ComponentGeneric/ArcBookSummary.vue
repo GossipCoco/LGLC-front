04bcd51731d5a8174b3d718320fe06e9e54b5fb4
GossipCoco <gossipcoco@hotmail.fr>
2025-05-09 16:43:00 +0200

    fix request refracto arc book dashboard new component

---
+++ src/components/Dashboard/ComponentGeneric/ArcBookSummary.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card-body new-current-arc-container padding-2-vh text-white height-30-vh">
+        <h2 class="align-items-content-justify-content">
+        <router-link            
+        class="poppins-text color-clear-green-01"
+        :to="'/OneArc/'+ Id">
+                {{ Title }}
+            </router-link>
+        </h2>
+        <div class="summary-book-dashboard display-flex-row align-items-content-justify-content width-70-vh margin--0-0--4">
+        <img :src="Image" width="20%" class="border-radius-12px"/>
+        <div v-html="Summary" class="text-white text-align-justify overflowY-scroll summmary-book-height-dashboard poppins-text text-white padding-1-vh"></div>
+        </div>
+    </div>    
+</template>
+<script>
+export default {
+    name: 'ArcBookSummary',
+    props: ['route', 'Id', 'Title', 'Image', 'Summary']
+}
+</script>
