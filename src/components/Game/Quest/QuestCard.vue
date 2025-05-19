9fe8d29e60641bcd900eacb2a3dad9fae51740a0
GossipCoco <gossipcoco@hotmail.fr>
2025-05-19 15:45:58 +0200

    fix refracto fix quest component

--- src/components/Game/Quest/QuestCard.vue
+++ src/components/Game/Quest/QuestCard.vue
@@ -9,41 +9,18 @@
           class="background-image-quest width-15-vh background-size-cover flex-one"
           v-bind:style="{ backgroundImage: 'url(' + image.Image + ')' }"
         ></div>
-        <div class="display-flex-column one-quest flex-one">
-          <div
-            class="display-flex-column quest-button-text height-30-vh
-            align-items-content-justify-content vertical-align-middle"
-          >
-            <p
-              class="display-flex-column quest-description height-5-vh width-20-vh
-              font-weight-400 white-text display-none-responsive poppins-text"
-            >
-              Difficulté : {{ quest.DifficultyLevel }}
-            </p>
-             <p
-              class="display-flex-column quest-description height-40-vh width-20-vh
-              font-weight-400 white-text display-none-responsive poppins-text"
-            >
-              {{ quest.Description }}
-            </p>
-            <p class="display-flex-column quest-button-link height-20-vh">
-              <router-link
-                :to="'/QuestById/' + quest.Id"
-                type="button"
-                class="display-flex-column btn btn-primary margin-4-0-0-0 width-20-vh poppins-text"
-              >
-                Relever le défi
-              </router-link>
-            </p>
-          </div>
-        </div>
+        <quest-mission-level v-bind:quest="quest" />
       </div>
     </div>
   </div>
 </template>
 <script>
+import QuestMissionLevel from './Components/QuestMissionLevel.vue';
 export default {
   name: "QuestCard",
   props: ["quest", "image"],
+  components:{
+    QuestMissionLevel
+  }
 };
 </script>
