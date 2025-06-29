413c9d2836bcae9125c66fc6f035dc521b9c8714
GossipCoco <gossipcoco@hotmail.fr>
2025-06-29 11:04:42 +0200

    fix refracto group

--- src/components/Group/GroupLayout.vue
+++ src/components/Group/GroupLayout.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="group-container"
-    class="height-80-vh border-none background-color-main-lineart flex-one card display-flex-column
+    class="height-80-vh border-none background-none  flex-one card display-flex-column
     margin-2vh padding-2-vh">
-    <CardHeader v-bind:Title="'Les clubs'" />
-    <div class="card-body padding-2-vh height-80-vh"></div>
+        <CardHeader v-bind:Title="'Les clubs'" />
+        <div class="card-body padding-2-vh height-80-vh"></div>
     </div>
 </template>
 <script>
