7a5e5321dd17bdca9152227c9a95d96a3f9c8a37
GossipCoco <gossipcoco@hotmail.fr>
2025-09-10 09:30:26 +0200

    group add component card

--- src/components/Group/GroupLayout.vue
+++ src/components/Group/GroupLayout.vue
@@ -1,36 +1,40 @@
 <template>
-    <div id="group-container"
-    class="height-80-vh border-none background-none  flex-one card display-flex-column margin-2vh padding-2-vh">
-        <CardHeader v-bind:Title="'Les clubs'" />
-        <div class="card-body padding-2-vh height-80-vh">
-            
-        </div>
+  <div
+    id="group-container"
+    class="height-80-vh border-none background-none flex-one card display-flex-column margin-2vh padding-2-vh"
+  >
+    <CardHeader v-bind:Title="'Les clubs'" />
+    <div class="card-body padding-2-vh height-80-vh">
+      <div class="row">
+        <div class="col-sm-6 mb-3 mb-sm-0"></div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import GroupService from '../../services/GroupService';
-import CardHeader from '../Components/GenericComponent/CardHeader.vue';
+import GroupService from "../../services/GroupService";
+import CardHeader from "../Components/GenericComponent/CardHeader.vue";
 export default {
-    name: 'GroupLayout',
-  components: { CardHeader},
-    data(){
-        return{
-            AllGroups:{}
-        }
-    },
-    created(){
-        this.GetAllGroups()
-    },
-    methods:{
-        GetAllGroups(){
-            GroupService.GetAllGroups()
-                .then((response) => {
-                    console.log(response.data.ob)
-                })
+  name: "GroupLayout",
+  components: { CardHeader },
+  data() {
+    return {
+      AllGroups: {},
+    };
+  },
+  created() {
+    this.GetAllGroups();
+  },
+  methods: {
+    GetAllGroups() {
+      GroupService.GetAllGroups()
+        .then((response) => {
+          console.log(response.data.ob);
+        })
         .catch((err) => {
           console.error("erreur", err);
         });
-        }
-    }
-}
+    },
+  },
+};
 </script>
