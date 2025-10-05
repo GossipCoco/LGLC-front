ed5a922962140a9af77fa24d7e93622f8b5cc110
GossipCoco <gossipcoco@hotmail.fr>
2025-10-05 10:54:23 +0200

    update server ip

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const Config = {};
 // Config.Url = "http://192.168.168.153:4047"
 // Config.Url = "http://192.168.1.23:4047"
 if (typeof window !== 'undefined') {
-  const hostname = 'http://192.168.1.11';
+  const hostname = 'http://192.168.1.12';
   const port = '4047'; // Assurez-vous que c'est le port sur lequel votre back-end écoute
   Config.Url = `${hostname}:${port}`;
 } else {
   // Si vous exécutez du code côté serveur (Node.js), vous pouvez définir une URL par défaut ou utiliser l'adresse IP du serveur
-  Config.Url = 'http://192.168.1.11:4047';
+  Config.Url = 'http://192.168.1.12:4047';
 }
 
 Config.secret = 'gossip-coco-key';
