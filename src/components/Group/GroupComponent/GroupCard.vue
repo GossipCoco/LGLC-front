dafbb7a929559540a5802857d783ed44bbc76793
GossipCoco <gossipcoco@hotmail.fr>
2025-10-18 14:06:20 +0200

    up group link color and card background

--- src/components/Group/GroupComponent/GroupCard.vue
+++ src/components/Group/GroupComponent/GroupCard.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="card background-color-dark-green-01 width-40-vh border-radius-top-100">
-        <div class="height-30-vh width-40-vh background-size-cover background-position-50-percent border-radius-top-100"
-          v-bind:style="{ backgroundImage: 'url(' + group.Image + ')',}"
-        >
-        <slot></slot>
+  <div
+    class="card background-color-dark-green-01 width-40-vh border-radius-top-100"
+  >
+    <div class="height-30-vh width-40-vh background-size-cover background-position-50-percent border-radius-top-100"
+      v-bind:style="{ backgroundImage: 'url(' + group.Image + ')' }"
+    >
+      <slot></slot>
     </div>
-    <!-- <img :src="group.Image" class="card-img-top" alt="..." /> -->
     <div class="card-body">
-      <router-link :to="'/OneGroupLayout/' + group.Id" class="text-white">{{
-        group.Name
-      }}</router-link>
+      <router-link :to="'/OneGroupLayout/' + group.Id" class="text-white">
+        {{ group.Name }}
+      </router-link>
     </div>
   </div>
 </template>
 <script>
-
 export default {
   props: ["group"],
   name: "GroupCard",
