3ef5314fc4251bbaea0891b6e8a6f46dcb9cca86
GossipCoco <gossipcoco@hotmail.fr>
2025-10-20 14:09:09 +0200

    up component header

--- src/components/Group/GroupComponent/GroupHeader.vue
+++ src/components/Group/GroupComponent/GroupHeader.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="row">
     <div class="col-12 col-md-12">
-        <div class="header-group-container">
-      <div
-        class="background-size-cover display-flex background-size-cover card-image flex-one border-card-color margin-1vh display-flex-row align-items-content-justify-content group-club-image height-20-vh width-185-vh"
-        v-bind:style="{
-          backgroundImage: 'url(' + group.Image + ')',
-        }"
-      >
-        <slot
-          ><h1>{{ group.Name }}</h1></slot
+      <div class="header-group-container">
+        <div
+          class="background-size-cover display-flex background-size-cover card-image flex-one border-card-color margin-1vh display-flex-row align-items-content-justify-content group-club-image height-20-vh width-185-vh"
+          v-bind:style="{
+            backgroundImage: 'url(' + group.Image + ')',
+          }"
         >
-      </div>
+          <slot
+            ><h1>{{ group.Name }}</h1></slot
+          >
+        </div>
       </div>
     </div>
   </div>
