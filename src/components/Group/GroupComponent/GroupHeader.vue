275c0bd53c1a6a8ec081a1c034fb24e635073691
GossipCoco <gossipcoco@hotmail.fr>
2025-10-20 18:15:29 +0200

    up component group

--- src/components/Group/GroupComponent/GroupHeader.vue
+++ src/components/Group/GroupComponent/GroupHeader.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-12 col-md-12">
-      
+    <div class="col-12 col-md-12">      
         <div
           class="background-size-cover display-flex background-size-cover card-image flex-one border-card-color margin-1vh display-flex-row align-items-content-justify-content group-club-image height-20-vh width-185-vh"
           v-bind:style="{
