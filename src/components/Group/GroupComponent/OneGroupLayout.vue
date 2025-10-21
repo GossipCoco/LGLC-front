376f2c519b4d55614ee43989eb42d41685a3e76e
GossipCoco <gossipcoco@hotmail.fr>
2025-10-21 15:24:56 +0200

    add row group

--- src/components/Group/GroupComponent/OneGroupLayout.vue
+++ src/components/Group/GroupComponent/OneGroupLayout.vue
@@ -8,7 +8,11 @@
       <div class="col-12 col-md-12">
         <div class="card border-none height-95-vh background-color-main-lineart details-infos-group border-radius-12px poppins-text text-white">          
           <div class="card-body">
-            <p>{{ group.Description }}</p>
+           <div class="row">
+            <div class="col-12 col-md-12">
+              <p>{{ group.Description }}</p>
+            </div>
+           </div>
             <div class="row">
               <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">                
                 <list-members v-bind:group="group"/>
