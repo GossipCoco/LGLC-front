a5050763f3707f8d5d5a7bf7613e3ca488fbc944
GossipCoco <gossipcoco@hotmail.fr>
2025-10-22 15:43:27 +0200

    up group componenent post fix

--- src/components/Group/GroupComponent/GroupLastPost.vue
+++ src/components/Group/GroupComponent/GroupLastPost.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4"  v-for="(posts, index) in Post" :key="index">
-    <div class="card">
+    <div class="card  background-lineart">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title text-white">{{ posts.Title }}</h5>
         <div>
-          <h2>{{ posts.Title }}</h2>
-          <p class="text-align-justify">
+          <p class="text-align-justify text-white">
             {{ truncateText(posts.Content, 255) }}
           </p>
           <p></p>
