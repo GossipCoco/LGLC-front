d9fbec92b8eef91894fd2b2c04f333c0f94eb15a
GossipCoco <gossipcoco@hotmail.fr>
2025-10-23 14:10:23 +0200

    up group componenent post fix

--- src/components/Group/PostCommentReactionComponent/CreateANewPost.vue
+++ src/components/Group/PostCommentReactionComponent/CreateANewPost.vue
@@ -1,19 +1,30 @@
 <template>
-    <div>
+<div class="card background-lineart height-90-vh">
+  <div class="card-header text-white">
+    <h1>{{ url }} </h1>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Créer un fil de discusssion</h5>
+    <p class="card-text">
         
-    </div>
+    </p>
+  </div>
+</div>
 </template>
 <script>
 export default {
     name: "CreateANewPost",
     data() {
         return {
-            url: null
+            url: null,
+            userCurrent: null
         };
     },
     created() {
         this.url = this.$route.params.id;
+         this.userCurrent = this.$store.state.auth.user.usrID;
         console.log("CreateANewPost created with url:", this.url);
+        console.log("Current user ID:", this.userCurrent);
     }   
 }
 </script>
