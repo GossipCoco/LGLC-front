8a4dde086e0ead23ad5f4a4d4eb521c9ba17856e
GossipCoco <gossipcoco@hotmail.fr>
2025-10-23 12:21:41 +0200

    up group componenent post fix

--- src/components/Group/PostCommentReactionComponent/CreateANewPost.vue
+++ src/components/Group/PostCommentReactionComponent/CreateANewPost.vue
@@ -1,8 +1,19 @@
 <template>
-    <div></div>
+    <div>
+        
+    </div>
 </template>
 <script>
 export default {
     name: "CreateANewPost",
+    data() {
+        return {
+            url: null
+        };
+    },
+    created() {
+        this.url = this.$route.params.id;
+        console.log("CreateANewPost created with url:", this.url);
+    }   
 }
 </script>
