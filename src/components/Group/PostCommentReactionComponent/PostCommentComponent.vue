3fbbeca882a799a8cf12cf86fbd6b8bc1a32cc3b
GossipCoco <gossipcoco@hotmail.fr>
2025-10-25 14:23:29 +0200

    up group componenent post component get data

--- src/components/Group/PostCommentReactionComponent/PostCommentComponent.vue
+++ src/components/Group/PostCommentReactionComponent/PostCommentComponent.vue
@@ -3,8 +3,9 @@
     id="post-comment-details"
     class="width-190-vh height-100-vh background-none border-none padding-2-vh card mb-3 overflowY-auto"
   >
+    <group-header v-bind:group="postCommentReactions.Group" />
     <div
-      class="card group-container border-none height-120-vh background-none details-infos-group border-radius-12px poppins-text text-white"
+      class="card group-container border-none height-120-vh background-lineart details-infos-group border-radius-12px poppins-text text-white"
     >
       <div class="card-body">
 
@@ -14,9 +15,11 @@
 </template>
 <script>
 import PostCommentReactionsService from "../../../services/PostCommentReactions";
+import GroupHeader from "../GroupComponent/GroupHeader.vue";
 export default {
   name: "PostCommentReactionComponent",
   props: ["PostCommentReactions"],
+    components: { GroupHeader },
   data() {
     return {
       url: "",
@@ -34,7 +37,8 @@ export default {
         PostCommentReactionsService.GetAPostAllCommentReactionsById(id)
         .then((response) => {
           this.postCommentReactions = response.data.ob;
-          console.log(this.postCommentReactions);
+          console.log(this.postCommentReactions);          
+          console.log(this.postCommentReactions.Group);
         })
     }
    },
