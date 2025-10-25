19814700b1c4296caab6e5371741b3e2cb717ddc
GossipCoco <gossipcoco@hotmail.fr>
2025-10-25 13:42:00 +0200

    up group componenent btn group

--- src/components/Group/GroupComponent/GroupLastPost.vue
+++ src/components/Group/GroupComponent/GroupLastPost.vue
@@ -2,7 +2,10 @@
   <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4"  v-for="(posts, index) in Post" :key="index">
     <div class="card  background-lineart">
       <div class="card-body">
-        <h5 class="card-title text-white">{{ posts.Title }}</h5>
+        <div class="display-flex display-flex-column align-items-content-justify-content margin-bottom-1vh">
+          <h2 class="card-title text-white">{{ posts.Title }}</h2>
+          <h5 class="card-title text-white">{{ posts.User.UserName }}</h5>
+        </div>
         <div>
           <p class="text-align-justify text-white">
             {{ truncateText(posts.Content, 255) }}
