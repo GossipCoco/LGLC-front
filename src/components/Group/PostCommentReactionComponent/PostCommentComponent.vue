49245c8815470a37ccea3a5bb2616b992df53f4f
GossipCoco <gossipcoco@hotmail.fr>
2025-10-27 08:18:02 +0100

    up component post content comment

--- src/components/Group/PostCommentReactionComponent/PostCommentComponent.vue
+++ src/components/Group/PostCommentReactionComponent/PostCommentComponent.vue
@@ -14,7 +14,7 @@
               <div class="card-header">
                 <div class="row">
                   <div class="col-8">
-                    <h2 class="text-white post-title-text">{{ postCommentReactions.Title }}</h2>
+                    <h2 class="text-white post-title-text">POST : {{ postCommentReactions.Title }}</h2>
                   </div>
                   <div class="col-4 text-align-right">
                     <p class="text-white">{{ UserName }}</p>
@@ -32,16 +32,15 @@
         </div>
         <div class="row" v-for="(Comments, index) in Comments" :key="index">
             <div class="card background-color-middle-green-01">
-              <!-- <div class="card-header">
+              <div class="card-header">
                 <div class="row">
                   <div class="col-8">
-                    <h2 class="text-white post-title-text">{{ postCommentReactions.Title }}</h2>
+                    <h2 class="text-white post-title-text">Réponse</h2>
                   </div>
-                  <div class="col-4 text-align-right">
-                    <p class="text-white">{{ UserName }}</p>
+                  <div class="col-4 text-align-right">                    
                   </div>
                 </div>
-              </div> -->
+              </div>
               <div class="card-body ">
                 <p
                   class="text-align-justify text-white"
