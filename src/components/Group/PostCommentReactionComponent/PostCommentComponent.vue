d863a735b660f9ea125cd880e83198c2b8049f2d
GossipCoco <gossipcoco@hotmail.fr>
2025-10-27 08:33:04 +0100

    up component post fix

--- src/components/Group/PostCommentReactionComponent/PostCommentComponent.vue
+++ src/components/Group/PostCommentReactionComponent/PostCommentComponent.vue
@@ -9,28 +9,6 @@
     >
       <div class="card-body">
         <PostCardComponent v-bind:post="postCommentReactions" v-bind:UserName="UserName" />
-        <!-- <div class="row">
-          <div class="col-12">
-            <div class="card background-color-dark-green-01 margin-1vh">
-              <div class="card-header">
-                <div class="row">
-                  <div class="col-8">
-                    <h2 class="text-white post-title-text">POST : {{ postCommentReactions.Title }}</h2>
-                  </div>
-                  <div class="col-4 text-align-right">
-                    <p class="text-white">{{ UserName }}</p>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body ">
-                <p
-                  class="text-align-justify text-white"
-                  v-html="postCommentReactions.Content"
-                ></p>
-              </div>
-            </div>
-          </div>
-        </div> -->
         <div class="row margin-1vh" v-for="(Comments, index) in Comments" :key="index"  >
             <div class="card background-color-middle-green-01">
               <div class="card-header">
