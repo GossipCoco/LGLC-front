8a8acf18950c6154f8ff387b20fc9743924fc7f7
GossipCoco <gossipcoco@hotmail.fr>
2025-11-02 13:06:32 +0100

    up component group comment component up

--- src/components/Group/PostCommentReactionComponent/PostCommentComponent.vue
+++ src/components/Group/PostCommentReactionComponent/PostCommentComponent.vue
@@ -10,25 +10,6 @@
       <div class="card-body">
         <PostCardComponent v-bind:post="postCommentReactions" v-bind:UserName="UserName" />
         <comments-component v-bind:Comments="Comments" />
-        <!-- <div class="row margin-1vh" v-for="(Comments, index) in Comments" :key="index"  >
-            <div class="card background-color-middle-green-01">
-              <div class="card-header">
-                <div class="row">
-                  <div class="col-8">
-                    <h2 class="text-white post-title-text">Réponse</h2>
-                  </div>
-                  <div class="col-4 text-align-right">                    
-                  </div>
-                </div>
-              </div>
-              <div class="card-body ">
-                <p
-                  class="text-align-justify text-white"
-                  v-html="Comments.Content"
-                ></p>
-              </div>
-            </div>
-        </div> -->
       </div>
     </div>
   </div>
