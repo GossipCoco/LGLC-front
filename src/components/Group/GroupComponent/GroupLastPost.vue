9da24622f3e1f9ef10b0e688756b149d5d6c7b7a
GossipCoco <gossipcoco@hotmail.fr>
2025-11-07 19:28:43 +0100

    up group add allpostbygroup posts css

--- src/components/Group/GroupComponent/GroupLastPost.vue
+++ src/components/Group/GroupComponent/GroupLastPost.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4"  v-for="(posts, index) in Post" :key="index">
-    <div class="card background-lineart height-40-vh">
+  <div
+    class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3"
+    v-for="(posts, index) in Post"
+    :key="index"
+  >
+    <div class="card background-lineart height-70-vh">
+      <div
+        class="card-header text-center text-white width-40-vh height-20-vh display-flex display-flex-column flex-wrap-wrap"
+      >
+        <h4 class="card-title text-white">{{ posts.Title }}</h4>
+        <p class="card-title text-white font-size-0-65">
+          {{ posts.User.UserName }}
+        </p>
+      </div>
       <div class="card-body">
-        <div class="display-flex display-flex-column align-items-content-justify-content margin-bottom-1vh">
-          <h2 class="card-title text-white">{{ posts.Title }}</h2>
-          <p class="card-title text-white font-size-0-65">{{ posts.User.UserName }}</p>
-        </div>
-        <div>
-          <p class="text-align-justify text-white" v-html="truncateText(posts.Content, 255)">  
-          </p>
-          <p>
-            <router-link :to="'/PostComment/' + posts.Id" class="btn btn-primary"
-              >Voir plus</router-link>
-          </p>
-        </div>
+        <p
+          class="text-align-justify text-white"
+          v-html="truncateText(posts.Content, 255)"
+        ></p>
+        <p>
+          <router-link :to="'/PostComment/' + posts.Id" class="btn btn-primary"
+            >Voir plus</router-link
+          >
+        </p>
       </div>
     </div>
   </div>
