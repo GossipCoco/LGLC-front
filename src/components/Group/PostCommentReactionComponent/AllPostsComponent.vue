1c11ab432f7a4e8864ab3a2cb20d7ffa34e9f346
GossipCoco <gossipcoco@hotmail.fr>
2025-11-07 12:46:56 +0100

    up group add allpostbygroup component

--- src/components/Group/PostCommentReactionComponent/AllPostsComponent.vue
+++ src/components/Group/PostCommentReactionComponent/AllPostsComponent.vue
@@ -1,31 +1,42 @@
 <template>
-    <div></div>
+  <div
+    id="group-details"
+    class="width-190-vh height-100-vh background-none border-none padding-2-vh card mb-3 overflowY-auto"
+  >
+  <group-header v-bind:group="Group" />
+   
+  </div>
 </template>
 <script>
 import PostAllCommentReactions from "../../../services/PostCommentReactions";
+import GroupHeader from "../GroupComponent/GroupHeader.vue";
 export default {
-    name: "AllPostsComponent",
-    data() {
-        return {
-            url: "",
-            Post: {},
-        };
+  name: "AllPostsComponent",
+  components: {
+    GroupHeader,
+  },
+  data() {
+    return {
+      url: "",
+      Post: {},
+      Group: {},
+    };
+  },
+  created() {
+    this.url = this.$route.params.id;
+    this.GetAllPostsByGroupId(this.url);
+  },
+  methods: {
+    GetAllPostsByGroupId(id) {
+      PostAllCommentReactions.GetAllPostsByGroupId(id)
+        .then((response) => {
+          this.Group = response.data.ob;
+          console.log(this.Group);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     },
-    created() {
-        this.url = this.$route.params.id;
-        this.GetAllPostsByGroupId(this.url);
-    },
-    methods: {
-        GetAllPostsByGroupId(id) {
-            PostAllCommentReactions.GetAllPostsByGroupId(id)
-                .then((response) => {
-                    this.Post = response.data.ob;
-                    console.log(this.Post);
-                })
-                .catch((e) => {
-                    console.log(e);
-                });
-        },
-    },
-}
+  },
+};
 </script>
