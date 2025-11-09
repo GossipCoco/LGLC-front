c7cf1d6f89292cd954d564e95b2cc1c1eedb4be6
GossipCoco <gossipcoco@hotmail.fr>
2025-11-09 10:09:39 +0100

    up create a new response component

--- src/components/Group/GroupComponent/GroupLastPost.vue
+++ src/components/Group/GroupComponent/GroupLastPost.vue
@@ -4,25 +4,36 @@
     v-for="(posts, index) in Post"
     :key="index"
   >
-    <div class="card background-lineart height-70-vh">
+    <div class="card background-lineart height-50-vh">
       <div
         class="card-header text-center text-white width-40-vh height-20-vh display-flex display-flex-column flex-wrap-wrap"
       >
         <h4 class="card-title text-white">{{ posts.Title }}</h4>
         <p class="card-title text-white font-size-0-65">
           {{ posts.User.UserName }}
+          <small class="opacity-75">{{ formatDate(posts.CreatedAt) }}</small>
+          <span v-if="posts.IsPinned" class="ms-auto badge bg-success"
+            >📌 Épinglé</span
+          >
         </p>
       </div>
       <div class="card-body">
-        <p
-          class="text-align-justify text-white"
-          v-html="truncateText(posts.Content, 255)"
-        ></p>
-        <p>
-          <router-link :to="'/PostComment/' + posts.Id" class="btn btn-primary"
-            >Voir plus</router-link
-          >
-        </p>
+        <div class="row">
+          <div class="col-12 mb-3 height-20-vh">
+            <p v-html="truncateText(posts.Content, 200)" class="text-white"></p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 mb-3">
+            <p>
+              <router-link
+                :to="'/PostComment/' + posts.Id"
+                class="btn btn-primary"
+                >Voir plus</router-link
+              >
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -32,6 +43,13 @@ export default {
   name: "GroupLastPost",
   props: ["Post"],
   methods: {
+    formatDate(d) {
+      try {
+        return new Date(d).toLocaleString();
+      } catch {
+        return d || "";
+      }
+    },
     truncateText(text, maxLength) {
       if (text.length <= maxLength) {
         return text;
