ad4d17163a20fe85feda5a8bc204fc11492df9ce
GossipCoco <gossipcoco@hotmail.fr>
2025-11-10 10:01:35 +0100

    up post comment update

--- src/components/Group/PostCommentReactionComponent/NewResponseToCommentComponent.vue
+++ src/components/Group/PostCommentReactionComponent/NewResponseToCommentComponent.vue
@@ -1,50 +1,63 @@
 <template>
-    <div
-        id="group-details"
-        class="width-190-vh height-100-vh background-none border-none padding-2-vh card mb-3 overflowY-auto"
-    >
-        <group-header v-bind:group="InfoGroup" />
-
-    </div>
+  <div
+    id="group-details"
+    class="width-190-vh height-100-vh background-none border-none padding-2-vh card mb-3 overflowY-auto"
+  >
+    <group-header v-bind:group="InfoGroup" />
+  </div>
 </template>
 <script>
-import GroupService from '../../../services/GroupService';
-import GroupHeader from '../GroupComponent/GroupHeader.vue';
+import GroupService from "../../../services/GroupService";
+import GroupHeader from "../GroupComponent/GroupHeader.vue";
 export default {
-    name: "NewResponseToCommentComponent",
-    components: {
-        GroupHeader,
-    },  
-    data() {
-        return {
-            url: null,
-            Group:{},
-            InfoGroup:{},
-            form:{
-                Content: "", // HTML renvoyé par l’éditeur
-                UserId: this.$store.state.auth.user.usrID,
-                PostId: null,
-                ParentId: this.$route.params.id,
-            }
-        };
+  name: "NewResponseToCommentComponent",
+  components: {
+    GroupHeader,
+  },
+  data() {
+    return {
+      url: null,
+      Group: {},
+      InfoGroup: {},
+      Background: null,
+      form: {
+        Content: "", // HTML renvoyé par l’éditeur
+        UserId: this.$store.state.auth.user.usrID,
+        PostId: null,
+        ParentId: this.$route.params.id,
+      },
+    };
+  },
+  created() {
+    this.url = this.$route.params.id;
+    this.GetGroupByCommentById(this.url);
+  },
+  computed: {
+    bgStyle() {
+      const src = this.Background;
+      console.log("Background source:", src);
+      if (!src) return {};
+      return {
+        backgroundImage: `url("${src}")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+      };
     },
-    created() {
-        this.url = this.$route.params.id;
-        this.GetGroupByCommentById(this.url);
+  },
+  methods: {
+    GetGroupByCommentById(id) {
+      GroupService.GetGroupByCommentById(id)
+        .then((response) => {
+          this.Group = response.data.ob;
+          console.log(this.Group);
+          this.InfoGroup = this.Group.GroupPost.Group;
+          this.form.PostId = this.Group.GroupPost.Id;
+          this.Background = this.InfoGroup.Background;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     },
-    methods: {
-        GetGroupByCommentById(id) {
-            GroupService.GetGroupByCommentById(id)
-            .then((response) => {
-                this.Group = response.data.ob;
-                console.log(this.Group);
-                this.InfoGroup = this.Group.GroupPost.Group;
-                this.form.PostId = this.Group.GroupPost.Id;
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-        }
-    }
-}
+  },
+};
 </script>
