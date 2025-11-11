1fc7ea59415017068d2d7d7493acec6f1ed3d354
GossipCoco <gossipcoco@hotmail.fr>
2025-11-11 13:39:58 +0100

    up group layout

--- src/components/Group/GroupComponent/GroupHeader.vue
+++ src/components/Group/GroupComponent/GroupHeader.vue
@@ -10,11 +10,11 @@
         <div class="header-group-container">
           <slot>
             <div class="row">
-              <div class="col-6 padding-20vh">
+              <div class="col-8 padding-20vh">
                 <h1 class="text-white">{{ group.Name }}</h1> <br>
                 <h3 class="text-white">{{ group.Subtitle }}</h3>
               </div>              
-              <div class="col-6">
+              <div class="col-4">
                 <img :src="group.Symbol" class="height-40-vh"/>
               </div>
             </div>            
