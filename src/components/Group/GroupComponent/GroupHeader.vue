89c0b1455027a3488a6a31ae0bb3e1a4ff13544f
GossipCoco <gossipcoco@hotmail.fr>
2025-11-11 18:09:56 +0100

    up group layout

--- src/components/Group/GroupComponent/GroupHeader.vue
+++ src/components/Group/GroupComponent/GroupHeader.vue
@@ -10,7 +10,7 @@
         <div class="header-group-container">
           <slot>
             <div class="row">
-              <div class="col-8 padding-20vh">
+              <div class="col-8">
                 <h1 class="text-white">{{ group.Name }}</h1> <br>
                 <h3 class="text-white">{{ group.Subtitle }}</h3>
               </div>              
