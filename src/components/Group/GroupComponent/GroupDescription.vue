4333fb29f97e40c91e7422c20abae5b1abd9ce4b
GossipCoco <gossipcoco@hotmail.fr>
2025-11-11 14:03:51 +0100

    up group layout

--- src/components/Group/GroupComponent/GroupDescription.vue
+++ src/components/Group/GroupComponent/GroupDescription.vue
@@ -5,7 +5,8 @@
           <h5 class="card-title">Description</h5>
         </div>
         <div class="card-body">
-          <p class="text-white" v-html="group.CompleteDescription"></p>
+          <p class="text-white">{{ group.Description }}</p>
+          <p class="text-white text-align-justify" v-html="group.CompleteDescription"></p>
         </div>
       </div>
     </div>
