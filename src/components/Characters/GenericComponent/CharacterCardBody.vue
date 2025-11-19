ec06b42e2b116c598ba1f227e71422d970e0a165
GossipCoco <gossipcoco@hotmail.fr>
2025-11-19 10:09:02 +0100

    up fix css

--- src/components/Characters/GenericComponent/CharacterCardBody.vue
+++ src/components/Characters/GenericComponent/CharacterCardBody.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="card-body height-7-vh maR-0-5vh align-items-content-justify-content
+    <div class="card-body height-7-vh padding-0-0-0-3vh align-items-content-justify-content
     name-characatrer-link">
       <router-link
         :to="route + name"
         type="button"
-        class="btn btn-primary poppins-text background-color-active height-6-vh width-30-vh marginTR--6-2-5-vh"
+        class="btn btn-primary poppins-text background-color-active height-5-vh width-30-vh marginTR--6-2-5-vh"
       >
           {{ name }}
         </router-link>
