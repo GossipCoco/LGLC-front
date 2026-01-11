cbef9ee3ff21b35a560f0abbc7dfe18b53597156
GossipCoco <gossipcoco@hotmail.fr>
2026-01-11 08:29:51 +0100

    up responsive design fix

--- src/components/Characters/CharacterComponent/CharacterCard.vue
+++ src/components/Characters/CharacterComponent/CharacterCard.vue
@@ -2,7 +2,7 @@
   <div class="col-xxl-10 col-xl-10 col-lg-10 col-md-10 col-sm-10 col-xs-12">
     <div id="all-characters-container-card" class="row all-characters-container-card width-150-vh">
       <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 display-flex-row
-                  align-items-content-justify-content height-95-vh character-container overflowY-X-hidden">
+                  align-items-content-justify-content height-85 character-container overflowY-X-hidden">
                   
         <div
           v-for="(character, index) in characters_props"
