04a66ac941749eb85ca0c6aed39cca51c464ee08
GossipCoco <gossipcoco@hotmail.fr>
2026-01-12 13:42:01 +0100

    up responsive design fix

--- src/components/Characters/CharacterComponent/MyCharactersCard.vue
+++ src/components/Characters/CharacterComponent/MyCharactersCard.vue
@@ -11,7 +11,7 @@
                 >
                     <div class="card-group display-flex height-35-vh width-35-vh">
                         <div class="card individual-character-card background-position-50-percent
-                        height-40-vh background-lineart margin-1vh padding-1-vh">
+                        height-35-vh background-lineart margin-1vh">
                             <BackgroundImage v-bind:Image="character.Image" />
                             <CharacterCardBody
                             v-bind:id="character.Id"
