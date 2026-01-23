8020fe246735ecb4f1db6381a0a7cbd4b5fa9a1d
GossipCoco <gossipcoco@hotmail.fr>
2026-01-23 14:49:00 +0100

    up responsive design fix detail character tabpanel class css

--- src/components/Characters/GenericComponent/TableInformationCharacter.vue
+++ src/components/Characters/GenericComponent/TableInformationCharacter.vue
@@ -2,24 +2,19 @@
   <div class="display-flex-column flex-one padding-2-vh margin-2vh-0">
     <div class="row width-100-100">
       <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-sm-12 col-xs-12">
-        <p class="text-white"><strong>Genre</strong></p>
-        <p class="text-white">{{ genre }}</p>
+        <p class="text-white"><strong>Genre</strong> : {{ genre }}</p>
       </div>
       <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-sm-12 col-xs-12">
-        <p class="text-white"><strong>Grade</strong></p>
-        <p class="text-white">{{ Grade }}</p>
+        <p class="text-white"><strong>Grade</strong> : {{ Grade }}</p>
       </div>
       <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-sm-12 col-xs-12">
-        <p class="text-white"><strong>Chaton</strong></p>
-        <p class="text-white">{{ kitty }}</p>
+        <p class="text-white"><strong>Chaton</strong> : {{ kitty }}</p>
       </div>
       <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-sm-12 col-xs-12">
-        <p class="text-white"><strong>Apprenti</strong></p>
-        <p class="text-white">{{ apprentice }}</p>
+        <p class="text-white"><strong>Apprenti</strong> : {{ apprentice }}</p>
       </div>
       <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-sm-12 col-xs-12">
-        <p class="text-white"><strong>Guerrier</strong></p>
-        <p class="text-white">{{ warrior }}</p>
+        <p class="text-white"><strong>Guerrier</strong> : {{ warrior }}</p>
       </div>
     </div>
   </div>
