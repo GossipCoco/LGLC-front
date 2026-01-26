0d4cfe7d9335df783fee8ee0163990e91ce6b97d
GossipCoco <gossipcoco@hotmail.fr>
2026-01-26 07:36:02 +0100

    up responsive design fix

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,13 @@ module.exports = {
           additionalData: `@import "./src/assets/bootstrap-overrides.scss";`
         }
       }
-    }
+    },
+      chainWebpack: (config) => {
+    config.plugin("define").tap((args) => {
+      args[0].__VUE_OPTIONS_API__ = JSON.stringify(true);
+      args[0].__VUE_PROD_DEVTOOLS__ = JSON.stringify(false);
+      args[0].__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = JSON.stringify(false);
+      return args;
+    });
+  },
   }
