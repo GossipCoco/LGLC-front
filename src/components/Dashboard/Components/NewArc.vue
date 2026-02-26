f81020d63e66ceb7f2fd3a41eda4fc9c7262cf49
GossipCoco <gossipcoco@hotmail.fr>
2026-02-26 15:20:35 +0100

    feat(dashboard): add row layout for  arc and books cards

--- src/components/Dashboard/Components/NewArc.vue
+++ src/components/Dashboard/Components/NewArc.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="row top-left-dashboard">
-    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 card-global">
-      <div class="card background-lineart border-none arc-card last-arc-container height-40-vh margin-0-0-0-1vh">
-        <TitleHeaderDashboard
-          v-bind:title="'Dernier arc en cours'"
-          v-bind:type="'chapters'"
-        />
-        <arc-book-summary
-          v-bind:route="'/OneArc/'"
-          v-bind:Id="Id"
-          v-bind:Title="Title"
-          v-bind:Image="Image"
-          v-bind:Summary="Summary"
-        />
-      </div>
-    </div>
+  <div
+    class="card background-lineart border-none arc-card last-arc-container height-40-vh margin-0-0-0-1vh"
+  >
+    <TitleHeaderDashboard
+      v-bind:title="'Dernier arc en cours'"
+      v-bind:type="'chapters'"
+    />
+    <arc-book-summary
+      v-bind:route="'/OneArc/'"
+      v-bind:Id="Id"
+      v-bind:Title="Title"
+      v-bind:Image="Image"
+      v-bind:Summary="Summary"
+    />
   </div>
 </template>
 <script>
@@ -23,30 +21,28 @@ import TitleHeaderDashboard from "../../Components/SpecificComponent/TitleHeader
 import ArcBookSummary from "../ComponentGeneric/ArcBookSummary.vue";
 export default {
   name: "NewArc",
-  components: { TitleHeaderDashboard,
-    ArcBookSummary
-   },
+  components: { TitleHeaderDashboard, ArcBookSummary },
   data() {
     return {
-        currentArc: null,
-        Id: null,
-        Image: null,
-        Title: null,
-        Summary: null
+      currentArc: null,
+      Id: null,
+      Image: null,
+      Title: null,
+      Summary: null,
     };
   },
   mounted() {
     ArcBookService.GetCurrentArc()
-      .then(response => {        
+      .then((response) => {
         this.currentArc = response.data.ob;
-        this.Id = this.currentArc.Id
-        this.Image = this.currentArc.Image
-        this.Title = this.currentArc.Title
-        this.Summary = this.currentArc.Summary
+        this.Id = this.currentArc.Id;
+        this.Image = this.currentArc.Image;
+        this.Title = this.currentArc.Title;
+        this.Summary = this.currentArc.Summary;
       })
-      .catch(err => {
+      .catch((err) => {
         console.error("Erreur lors de la récupération de l'arc :", err);
       });
-  }
+  },
 };
 </script>
