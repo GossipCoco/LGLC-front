f61e3234cc0b2d500496f5f16342355dc0db841c
GossipCoco <gossipcoco@hotmail.fr>
2026-02-26 15:01:48 +0100

    up css dashboard

--- src/components/Dashboard/Components/CharacterRandom.vue
+++ src/components/Dashboard/Components/CharacterRandom.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="row bottom-left-dashboard">
-  <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 card-global">
     <div class="card border-none background-lineart fiction-card random-characters-card width-120-vh height-45-vh padding-0 margin-0-rem">
       <TitleHeaderDashboard v-bind:title="'Personnages aléatoires'" v-bind:type="'myCharacters'"/>
       <div class="card-body flex-wrap-wrap display-flex-row align-items-content-justify-content">
-         <div class="display-flex-row align-items-content-justify-content height-15-vh margin-0-2vh-0-0" 
+          <div class="display-flex-row align-items-content-justify-content height-15-vh margin-0-2vh-0-0" 
             v-for="character in randomCharacters"
             :key="character.Id"
           >
@@ -15,11 +13,9 @@
               v-bind:CharacterImage="character.Image"
               v-bind:CurrentName="character.CurrentName"
               />
-            </div>
           </div>
-       </div>
       </div>
-  </div>
+    </div>
 </template>
 <script>
 import TitleHeaderDashboard from "../../Components/SpecificComponent/TitleHeaderDashboard.vue";
