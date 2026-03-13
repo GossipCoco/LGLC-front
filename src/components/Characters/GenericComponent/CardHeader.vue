b41f116d809714cb4c8242c8ea2784ceb6eb1175
GossipCoco <gossipcoco@hotmail.fr>
2026-03-13 15:13:50 +0100

    refracto(header character):width fix

--- src/components/Characters/GenericComponent/CardHeader.vue
+++ src/components/Characters/GenericComponent/CardHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="
         card-header border-radius-12px z-index-9999
-        background-color-dark-green-01 display-flex-row align-items-content-justify-content width-190-vh">
+        background-color-dark-green-01 display-flex-row align-items-content-justify-content width-100-100">
         <h1 class="text-white poppins-text">Tous les personnages</h1>
         <router-link
         to="/CreateAnOriginalCharacter"
