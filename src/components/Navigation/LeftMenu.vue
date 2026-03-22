8b42be5138c7e1b00268174d15c192fad3d3f87b
GossipCoco <gossipcoco@hotmail.fr>
2026-03-22 19:00:43 +0100

    CSS(refratco)delete double fix

--- src/components/Navigation/LeftMenu.vue
+++ src/components/Navigation/LeftMenu.vue
@@ -120,7 +120,7 @@ export default {
       showSubMenu: false,
       logo: "/images/Logos/lgdlc-logo-v5.png",
       menuClass:'menu-left background-lineart-menu d-flex display-flex-row flex-shrink-0 height-15-vh width-100-100 margin--0-0--2 scrollbar-color',
-      ulClass: 'nav nav-pills display-flex-row mb-auto display-flex align-items-content-justify-content width-100-100 padding-2-vh-0',
+      ulClass: 'nav nav-pills display-flex-row mb-auto display-flex align-items-content-justify-content width-100-100',
       lilevel1: 'li-level1 display-flex-column align-items-content-justify-content',
       activeClass:'menu-link--active',
       exactActiveClass:'menu-link--exact-active',
