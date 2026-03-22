f3a67f91224d653309614426a7a1cdcc0aa34c46
GossipCoco <gossipcoco@hotmail.fr>
2026-03-22 14:44:17 +0100

    color theme(scss css) color fix

--- src/components/Navigation/LeftMenu.vue
+++ src/components/Navigation/LeftMenu.vue
@@ -117,7 +117,7 @@ export default {
       isMenuOpen: false,
       showSubMenu: false,
       logo: "/images/Logos/LaGuerredesClansLogos2.png",
-      menuClass:'menu-left background-lineart-other03 d-flex display-flex-row flex-shrink-0 height-15-vh width-100-100 margin--0-0--2 scrollbar-color',
+      menuClass:'menu-left background-lineart-menu d-flex display-flex-row flex-shrink-0 height-15-vh width-100-100 margin--0-0--2 scrollbar-color',
       ulClass: 'nav nav-pills display-flex-row mb-auto display-flex align-items-content-justify-content width-100-100 padding-2-vh-0',
       lilevel1: 'li-level1 display-flex-column align-items-content-justify-content',
       activeClass:'menu-link--active',
