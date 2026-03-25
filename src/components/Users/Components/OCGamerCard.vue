c129c09bba8a822ac393f3b1078647092a4ec9c5
GossipCoco <gossipcoco@hotmail.fr>
2026-03-25 15:51:49 +0100

    files css(refrato)width height  css fix

--- src/components/Users/Components/OCGamerCard.vue
+++ src/components/Users/Components/OCGamerCard.vue
@@ -6,7 +6,7 @@
     <div class="row row-cols-1 row-cols-md-4 g-4">
       <div class="col-3" v-for="(gamer, index) in gamers" :key="index">        
           <div class="display-flex width-40-vh align-items-content-justify-content">
-            <div class="card border-none background-color-main-lineart display-flex align-items-content-justify-content">
+            <div class="card height-35-vh border-none background-color-main-lineart display-flex align-items-content-justify-content">
               <div class="card-header width-40-vh">
                 <h3 class="card-title cinzel margin-2vh-0-0-0">
                   <router-link :to="'/OriginaleCharacterDetails/' + gamer.CurrentName" class="text-white">
