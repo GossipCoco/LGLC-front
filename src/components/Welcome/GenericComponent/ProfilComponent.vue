dd5759822e0c329748bbd47bef705aa34337a8e8
GossipCoco <gossipcoco@hotmail.fr>
2026-06-01 08:41:59 +0200

    welcome layout(create): component generic fix

---
+++ src/components/Welcome/GenericComponent/ProfilComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+    name: "ProfilComponent"
+}
+</script>
--- src/components/Welcome/WelcomeLayout.vue
+++ src/components/Welcome/WelcomeLayout.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="container-fluid">
     <div class="row height-100-vh">
-      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-2 col-xxl-2"></div>
-      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-10 col-xxl-10"></div>
+      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-3 col-xxl-3">
+        <div class="card" style="width: 18rem">          
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card’s content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-9 col-xxl-9"></div>
     </div>
   </div>
 </template>
