8212361f0c870b98e33151fd5ef85b0f6ba76ded
GossipCoco <gossipcoco@hotmail.fr>
2026-06-01 07:07:56 +0200

    welcome layout(create): add id current user

--- src/components/Welcome/WelcomeLayout.vue
+++ src/components/Welcome/WelcomeLayout.vue
@@ -4,5 +4,11 @@
 <script>
 export default {
   name: "WelcomeLayout",
-  }
+  data() {
+    return {
+      showspinner: false,
+      usrId: this.$store.state.auth.user.usrID,
+    };
+  },
+};
 </script>
