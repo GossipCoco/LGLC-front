e54042a538e58dd7efaa49b8ebd4f0f805726f09
GossipCoco <gossipcoco@hotmail.fr>
2026-06-01 07:53:33 +0200

    welcome layout(create): add row col

--- src/components/Welcome/WelcomeLayout.vue
+++ src/components/Welcome/WelcomeLayout.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-2 col-xxl-2"></div>
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-10 col-xxl-10"></div>
+  </div>
 </template>
 <script>
 export default {
