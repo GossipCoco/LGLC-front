92d171c142150645724a0cc0a83d840a7c419f99
GossipCoco <gossipcoco@hotmail.fr>
2026-06-04 14:53:32 +0200

    welcome layout(create): component generic card fix

--- src/components/Welcome/GenericComponent/ProfilComponent.vue
+++ src/components/Welcome/GenericComponent/ProfilComponent.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="card" style="width: 18rem">
+  <div class="card background-color-dark-green-01" style="width: 18rem">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
-      <p class="card-text">
+      <p class="card-text white-text">
         Some quick example text to build on the card title and make up the bulk
         of the card’s content.
       </p>
