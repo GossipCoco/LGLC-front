a6fdd1fd665d0efd23a4109850204d3d1a3fbc0b
GossipCoco <gossipcoco@hotmail.fr>
2026-06-04 16:31:08 +0200

    welcome layout(create): component generic card fix

--- src/components/Welcome/GenericComponent/TitleWelcomeComponent.vue
+++ src/components/Welcome/GenericComponent/TitleWelcomeComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="width-80-vh mar-0-auto text-align-center">
         <h1 class="font-weight-600 color-ternary-green">Bienvenue sur votre tableau de bord</h1>
     </div>
 </template>
