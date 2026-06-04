2cb3adb5f92598f7f32c0051bb0cea460ac11d85
GossipCoco <gossipcoco@hotmail.fr>
2026-06-04 14:45:52 +0200

    welcome layout(create): component generic add title

---
+++ src/components/Welcome/GenericComponent/TitleWelcomeComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Bienvenue sur votre tableau de bord</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TitleWelcomeComponent'
+}
+</script>
--- src/components/Welcome/WelcomeLayout.vue
+++ src/components/Welcome/WelcomeLayout.vue
@@ -4,15 +4,29 @@
       <div class="col-sm-12 col-md-12 col-lg-12 col-xl-3 col-xxl-3">
         <profil-component />
       </div>
-      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-9 col-xxl-9"></div>
+      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-9 col-xxl-9">
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <title-welcome-component ></title-welcome-component>
+          </div>
+        </div>        
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <title-welcome-component ></title-welcome-component>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import ProfilComponent from './GenericComponent/ProfilComponent.vue';
+import TitleWelcomeComponent from './GenericComponent/TitleWelcomeComponent.vue'
 export default {
   name: "WelcomeLayout",
-  components: {ProfilComponent},
+  components: {ProfilComponent,
+    TitleWelcomeComponent
+  },
   data() {
     return {
       showspinner: false,
