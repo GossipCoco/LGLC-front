5e18a0ed79ed391ff8109289978cf4386a7a7c50
GossipCoco <gossipcoco@hotmail.fr>
2026-06-04 15:08:05 +0200

    welcome layout(create): component generic card fix

--- src/components/Welcome/GenericComponent/LastChapters.vue
+++ src/components/Welcome/GenericComponent/LastChapters.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card background-color-dark-green-01" style="width: 18rem">
     <div class="card-body">
-      <h5 class="card-title">Mes dernières fictions</h5>
+      <h5 class="card-title">Mes dernièrs chapitres</h5>
       <p class="card-text white-text">
         Some quick example text to build on the card title and make up the bulk
         of the card’s content.
