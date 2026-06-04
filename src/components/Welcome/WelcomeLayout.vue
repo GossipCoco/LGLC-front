a5c5483fc19e4d934500fcdb5e62cd34ed116232
GossipCoco <gossipcoco@hotmail.fr>
2026-06-04 14:50:49 +0200

    welcome layout(create): component generic add last fiction card + div

--- src/components/Welcome/WelcomeLayout.vue
+++ src/components/Welcome/WelcomeLayout.vue
@@ -7,12 +7,21 @@
       <div class="col-sm-12 col-md-12 col-lg-12 col-xl-9 col-xxl-9">
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <title-welcome-component ></title-welcome-component>
+            <title-welcome-component></title-welcome-component>
           </div>
-        </div>        
+        </div>
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <title-welcome-component ></title-welcome-component>
+            <div class="row">
+              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-2 col-xxl-2">
+                <last-fictions-component />
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-2 col-xxl-2"></div>
+              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-2 col-xxl-2"></div>
+              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-2 col-xxl-2"></div>
+              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-2 col-xxl-2"></div>
+              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-2 col-xxl-2"></div>
+            </div>
           </div>
         </div>
       </div>
@@ -20,12 +29,15 @@
   </div>
 </template>
 <script>
-import ProfilComponent from './GenericComponent/ProfilComponent.vue';
-import TitleWelcomeComponent from './GenericComponent/TitleWelcomeComponent.vue'
+import ProfilComponent from "./GenericComponent/ProfilComponent.vue";
+import TitleWelcomeComponent from "./GenericComponent/TitleWelcomeComponent.vue";
+import LastFictionsComponent from "./GenericComponent/LastFictionsComponent.vue";
 export default {
   name: "WelcomeLayout",
-  components: {ProfilComponent,
-    TitleWelcomeComponent
+  components: {
+    ProfilComponent,
+    TitleWelcomeComponent,
+    LastFictionsComponent,
   },
   data() {
     return {
