688fcad1b6539992bd5687ad4bf5c255d63a1463
GossipCoco <gossipcoco@hotmail.fr>
2026-06-05 10:31:10 +0200

    welcome layout(create): component generic card fix

--- src/components/Welcome/GenericComponent/EasyActionsComponents.vue
+++ src/components/Welcome/GenericComponent/EasyActionsComponents.vue
@@ -2,7 +2,7 @@
   <div class="card background-color-dark-green-01">
     <div class="card-body">
       <h5 class="card-title">Actions rapides</h5>
-      <p class="card-text white-text font-size-1em">
+      <p class="white-text font-size-1em">
         Actions rapides
       </p>      
     </div>
--- src/components/Welcome/GenericComponent/LastFictionsComponent.vue
+++ src/components/Welcome/GenericComponent/LastFictionsComponent.vue
@@ -2,7 +2,7 @@
   <div class="card background-color-dark-green-01">
     <div class="card-body">
       <h5 class="card-title">Mes dernières fictions</h5>
-      <p class="card-text white-text  font-size-1em">
+      <p class="white-text  font-size-1em">
         Some quick example text to build on the card title and make up the bulk
         of the card’s content.
       </p>
