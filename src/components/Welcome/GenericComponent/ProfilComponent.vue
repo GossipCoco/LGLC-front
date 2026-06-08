1955fcc8696cec3cf361709f4c5877e07151d189
GossipCoco <gossipcoco@hotmail.fr>
2026-06-08 08:33:42 +0200

    welcome layout(create):add info profil up

--- src/components/Welcome/GenericComponent/ProfilComponent.vue
+++ src/components/Welcome/GenericComponent/ProfilComponent.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="card background-color-dark-green-01 height-70-vh">
+  <div class="card background-color-dark-green-01 height-70-vh">    
+  <div class="card-header">
+    <h5 class="card-title">{{ UserName }}</h5>
+  </div>
     <div class="card-body">
-      <h5 class="card-title">{{ UserName }}</h5>
+      <img  loading="lazy"  :src="'/images/Avatars/' + Avatar" :alt="Avatar" 
+      class="'avatar-dahboard-container border-radius-100 display-flex-column width-12-vh height-12-vh" 
+      />
       <p class="white-text font-size-1em">
         Some quick example text to build on the card title and make up the bulk
         of the card’s content.
